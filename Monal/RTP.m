2948fa38a124b29f7299466da977dc39d5b8b9eb
anurodhp <anurodhp@gmail.com>
2011-08-28 22:57:59 -0400

    changed to compiled as objective-c++ removed a ton of junk files that got added.
    everything should build and link now.

--- Monal/RTP.m
+++ Monal/RTP.m
@@ -5,13 +5,20 @@
 //  Created by Anurodh Pokharel on 8/28/11.
 //  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 //
-/*
+
+
 #include "rtpsession.h"
 #include "rtpudpv4transmitter.h"
 #include "rtpipv4address.h"
 #include "rtpsessionparams.h"
 #include "rtperrors.h"
-*/
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <stdlib.h>
+#include  <stdio.h>
+
 
 #import "RTP.h"
 

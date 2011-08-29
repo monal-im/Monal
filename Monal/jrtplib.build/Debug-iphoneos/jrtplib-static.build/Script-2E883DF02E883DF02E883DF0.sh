be928f903e333378b5006abd2c887e8ece09ad59
anurodhp <anurodhp@gmail.com>
2011-08-28 22:57:10 -0400

    should build now and set to obj-c++

--- Monal/jrtplib.build/Debug-iphoneos/jrtplib-static.build/Script-2E883DF02E883DF02E883DF0.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/anurodhp/Desktop/jrtplib-3.9.0 -f /Users/anurodhp/Desktop/jrtplib-3.9.0/CMakeScripts/ReRunCMake.make
--- Monal/jrtplib.build/Debug/jrtplib-shared.build/Script-2EF1ACF02EF1ACF02EF1ACF0.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/anurodhp/Desktop/jrtplib-3.9.0/src -f /Users/anurodhp/Desktop/jrtplib-3.9.0/src/CMakeScripts/jrtplib-shared_postBuildPhase.make$CONFIGURATION all

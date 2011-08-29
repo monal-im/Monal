cdb54b923a950a4de158c6852b1226ee068ba3a9
Anurodh Pokharel <anurodhp@Shiva.local>
2011-08-29 00:12:20 -0400

    changed a ton of files to C++ type and it works!  static rtp lib builds and links with monal

---
+++ Monal/RTP.mm
@@ -0,0 +1,28 @@
+//
+//  RTP.m
+//  Monal
+//
+//  Created by Anurodh Pokharel on 8/28/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+
+#include "rtpsession.hh"
+#include "rtpudpv4transmitter.hh"
+#include "rtpipv4address.hh"
+#include "rtpsessionparams.hh"
+#include "rtperrors.hh"
+
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <stdlib.h>
+#include  <stdio.h>
+
+
+#import "RTP.hh"
+
+@implementation RTP
+
+@end
--- Monal/rtptypes.h
+++
@@ -1,3 +0,0 @@
-
-#include <stdint.h>
-#include <sys/types.h>

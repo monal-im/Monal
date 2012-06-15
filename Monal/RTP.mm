58a7d44e3c506b6bb46bdcba356f389f67e61df8
Anurodh Pokharel <info@monal.im>
2012-06-15 01:40:23 -0400

    reads and sends cpm file over rtp..

--- Monal/RTP.mm
+++ Monal/RTP.mm
@@ -37,60 +37,26 @@ void checkerror(int rtperr)
 
 -(void) RTPConnect:(NSString*) IP:(int) port; 
 {
-    jrtplib::RTPSession sess;
-	uint16_t portbase,destport;
-	uint32_t destip;
-	std::string ipstr([IP  cStringUsingEncoding:NSUTF8StringEncoding]);
-	int status,i,num;
-    
-    destport=port; 
-    portbase=port+2; 
-    
-    num=10; 
-    
-    
-	// Now, we'll create a RTP session, set the destination, send some
-	// packets and poll for incoming data.
-	
-	jrtplib::RTPUDPv4TransmissionParams transparams;
-	jrtplib::RTPSessionParams sessparams;
-	
-	// IMPORTANT: The local timestamp unit MUST be set, otherwise
-	//            RTCP Sender Report info will be calculated wrong
-	// In this case, we'll be sending 10 samples each second, so we'll
-	// put the timestamp unit to (1.0/10.0)
-	sessparams.SetOwnTimestampUnit(1.0/10.0);		
-	
-	sessparams.SetAcceptOwnPackets(true);
-	transparams.SetPortbase(portbase);
-	status = sess.Create(sessparams,&transparams);	
-	checkerror(status);
-	
-	jrtplib::RTPIPv4Address addr(destip,destport);
-	
-	status = sess.AddDestination(addr);
-	checkerror(status);
-	
-    
     //Instanciate an instance of the AVAudioSession object.
-    AVAudioSession * audioSession = [AVAudioSession sharedInstance];
+     AVAudioSession * audioSession = [AVAudioSession sharedInstance];
     //Setup the audioSession for playback and record. 
     //We could just use record and then switch it to playback leter, but
     //since we are going to do both lets set it up once.
-      NSError * error;
+    NSError * error;
     [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error: &error];
     //Activate the session
     [audioSession setActive:YES error: &error];
     
     //Begin the recording session.
- 
+    
     NSMutableDictionary* recordSetting = [[NSMutableDictionary alloc] init];
     [recordSetting setValue :[NSNumber numberWithInt:kAudioFormatALaw] forKey:AVFormatIDKey]; // PCMA Audio
     [recordSetting setValue:[NSNumber numberWithFloat:8000] forKey:AVSampleRateKey]; 
     [recordSetting setValue:[NSNumber numberWithInt: 1] forKey:AVNumberOfChannelsKey];
-
-  
-    recordedTmpFile = [NSURL fileURLWithPath:[NSTemporaryDirectory() stringByAppendingPathComponent: [NSString stringWithString: @"temp_voip.caf"]]];
+    
+    
+  recordedTmpFile = [NSURL fileURLWithPath:[NSTemporaryDirectory() stringByAppendingPathComponent: [NSString stringWithString: @"temp_voip.caf"]]];
+    
     
     debug_NSLog(@"Using File called: %@",recordedTmpFile);
     
@@ -104,21 +70,86 @@ void checkerror(int rtperr)
     //the subsstems so that when we actually say "record" it starts right away.
     [recorder prepareToRecord];
     //Start the actual Recording
-    [recorder record];
+    //[recorder record];
+    
     //There is an optional method for doing the recording for a limited time see 
-    [recorder recordForDuration:(NSTimeInterval) 3];
+    [recorder recordForDuration:(NSTimeInterval) 10];
     
     //load intodata
-    NSData* audioData= [NSData dataWithContentsOfURL:recordedTmpFile];
+    int max_packet=1054; 
+    NSError* errorPtr=nil; 
+    NSData* audioData=[NSData dataWithContentsOfFile:@"/Users/anurodhp/Desktop/temp_voip.caf"
+                                              options:NSDataReadingUncached error:&errorPtr ]; 
     
     
-	//for (i = 1 ; i <= num ; i++)
+    if(errorPtr!=nil)
+    {
+        debug_NSLog(@"data read error %@", errorPtr);
+    }
+    
+    debug_NSLog(@"NSdata length %d", [audioData length]);
+    //[NSData dataWithContentsOfURL:recordedTmpFile];
+    
+    int packet_num =[audioData length]/max_packet; 
+    
+    
+    jrtplib::RTPSession sess;
+	uint16_t portbase,destport;
+	uint32_t destip;
+	std::string ipstr([IP  cStringUsingEncoding:NSUTF8StringEncoding]);
+	int status,i;
+    
+    destport=port; 
+    portbase=port+2; 
+    
+   
+    destip = inet_addr(ipstr.c_str());
+    
+	// Now, we'll create a RTP session, set the destination, send some
+	// packets and poll for incoming data.
+	
+	jrtplib::RTPUDPv4TransmissionParams transparams;
+	jrtplib::RTPSessionParams sessparams;
+	
+	// IMPORTANT: The local timestamp unit MUST be set, otherwise
+	//            RTCP Sender Report info will be calculated wrong
+	// In this case, we'll be sending 10 samples each second, so we'll
+	// put the timestamp unit to (1.0/10.0)
+	sessparams.SetOwnTimestampUnit(1.0/packet_num);		
+	
+	sessparams.SetAcceptOwnPackets(true);
+	transparams.SetPortbase(portbase);
+	status = sess.Create(sessparams,&transparams);	
+	checkerror(status);
+	
+	jrtplib::RTPIPv4Address addr(destip,destport);
+	
+	status = sess.AddDestination(addr);
+	checkerror(status);
+	
+    
+    debug_NSLog(@" RTP to ip %d  IP %@ on port %d", destip,IP,  destport);
+    
+
+    
+	for (i = 1 ; i <= packet_num ; i++)
 	{
-		debug_NSLog(@"\nSending packet %d/%d\n",i,num);
+		
 		
 		// send the packet
 		
-        status = sess.SendPacket((void *)[audioData bytes],[audioData length],0,false,10);
+        int start=(i-1)*max_packet; 
+        int end=max_packet;
+        
+        if((end+start)>[audioData length]) end=[audioData length]-start ; 
+        
+       const void* bytes=[[audioData subdataWithRange:NSMakeRange(start, end)] bytes]; 
+        
+        
+        debug_NSLog(@"Sending packet %d/%d\n  starting %d sized %d "
+                    ,i,packet_num, start,  end );
+        
+        status = sess.SendPacket((void *)bytes,end,0,false,1);
 		checkerror(status);
 		
 		sess.BeginDataAccess();
@@ -152,7 +183,7 @@ void checkerror(int rtperr)
 		jrtplib::RTPTime::Wait(jrtplib::RTPTime(1,0));
 	}
 	
-	//sess.BYEDestroy(jrtplib::RTPTime(10,0),0,0);
+	sess.BYEDestroy(jrtplib::RTPTime(10,0),0,0);
 }
 
 @end

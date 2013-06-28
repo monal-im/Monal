5d21435d275de5fa0a984c219a454d326d787d32
Anurodh Pokharel <anurodhp@alum.bu.edu>
2013-06-27 21:40:04 -0400

    added appirater

--- Monal/Classes/MonalAppDelegate.m
+++ Monal/Classes/MonalAppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "MonalAppDelegate.h"
 
+
+
 // tab bar
 #import "ContactsViewController.h"
 #import "ActiveChatsViewController.h"
@@ -21,17 +23,15 @@
 
 @implementation MonalAppDelegate
 
-
-- (void)applicationDidFinishLaunching:(UIApplication *)application
+-(void) createRootInterface
 {
-    
     self.window=[[UIWindow alloc] init];
     self.window.screen=[UIScreen mainScreen];
-
+    
     _tabBarController=[[UITabBarController alloc] init];
     ContactsViewController* contactsVC = [[ContactsViewController alloc] init];
-   
-     ActiveChatsViewController* activeChatsVC = [[ActiveChatsViewController alloc] init];
+    
+    ActiveChatsViewController* activeChatsVC = [[ActiveChatsViewController alloc] init];
     UINavigationController* activeChatNav=[[UINavigationController alloc] initWithRootViewController:activeChatsVC];
     activeChatNav.navigationBar.tintColor=[UIColor blackColor];
     activeChatNav.tabBarItem = [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"Active Chats",@"") image:[UIImage imageNamed:@"active"] tag:0];
@@ -41,7 +41,7 @@
     settingsNav.navigationBar.tintColor=[UIColor blackColor];
     settingsNav.tabBarItem = [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"Settings",@"") image:[UIImage imageNamed:@"status"] tag:0];
     
-     AccountsViewController* accountsVC = [[AccountsViewController alloc] init];
+    AccountsViewController* accountsVC = [[AccountsViewController alloc] init];
     UINavigationController* accountsNav=[[UINavigationController alloc] initWithRootViewController:accountsVC];
     accountsNav.navigationBar.tintColor=[UIColor blackColor];
     accountsNav.tabBarItem = [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"Accounts",@"") image:[UIImage imageNamed:@"accounts"] tag:0];
@@ -72,38 +72,55 @@
     aboutNav.tabBarItem = [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"About",@"") image:[UIImage imageNamed:@"about"] tag:0];
     
     
- if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone)
- {
-     
-     _chatNav=[[UINavigationController alloc] initWithRootViewController:contactsVC];
-     _chatNav.tabBarItem = [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"Contacts",@"") image:[UIImage imageNamed:@"Buddies"] tag:0];
-     
-     _tabBarController.viewControllers=[NSArray arrayWithObjects:_chatNav,activeChatNav, settingsNav,  accountsNav, chatLogNav, groupChatNav, searchUsersNav, helpNav, aboutNav, nil];
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone)
+    {
+        
+        _chatNav=[[UINavigationController alloc] initWithRootViewController:contactsVC];
+        _chatNav.tabBarItem = [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"Contacts",@"") image:[UIImage imageNamed:@"Buddies"] tag:0];
+        
+        _tabBarController.viewControllers=[NSArray arrayWithObjects:_chatNav,activeChatNav, settingsNav,  accountsNav, chatLogNav, groupChatNav, searchUsersNav, helpNav, aboutNav, nil];
+        
+        self.window.rootViewController=_tabBarController;
+        
+    }
+    else
+    {
+        
+        //this is a dummy nav controller not really used for anything
+        UINavigationController* navigationControllerContacts=[[UINavigationController alloc] initWithRootViewController:contactsVC];
+        navigationControllerContacts.navigationBar.tintColor=[UIColor blackColor];
+        
+        _chatNav=activeChatNav;
+        _splitViewController=[[UISplitViewController alloc] init];
+        self.window.rootViewController=_splitViewController;
+        
+        _tabBarController.viewControllers=[NSArray arrayWithObjects: activeChatNav,  settingsNav, accountsNav, chatLogNav, groupChatNav, searchUsersNav,  helpNav, aboutNav, nil];
+        
+        _splitViewController.viewControllers=[NSArray arrayWithObjects:navigationControllerContacts, _tabBarController,nil];
+        _splitViewController.delegate=self;
+    }
     
-    self.window.rootViewController=_tabBarController;
-     
- }
- else
- {
-     
-     //this is a dummy nav controller not really used for anything
-     UINavigationController* navigationControllerContacts=[[UINavigationController alloc] initWithRootViewController:contactsVC];
-    navigationControllerContacts.navigationBar.tintColor=[UIColor blackColor];
-     
-     _chatNav=activeChatNav; 
-    _splitViewController=[[UISplitViewController alloc] init];
-     self.window.rootViewController=_splitViewController;
-     
-     _tabBarController.viewControllers=[NSArray arrayWithObjects: activeChatNav,  settingsNav, accountsNav, chatLogNav, groupChatNav, searchUsersNav,  helpNav, aboutNav, nil];
-     
-     _splitViewController.viewControllers=[NSArray arrayWithObjects:navigationControllerContacts, _tabBarController,nil];
-     _splitViewController.delegate=self; 
- }
-    
-     _chatNav.navigationBar.tintColor=[UIColor blackColor];
+    _chatNav.navigationBar.tintColor=[UIColor blackColor];
     _tabBarController.moreNavigationController.navigationBar.tintColor=[UIColor blackColor];
-   
+    
     [self.window makeKeyAndVisible];
+}
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application
+{
+ [self createRootInterface];
+
+    //rating
+    [Appirater setAppId:@"317711500"];
+    
+    [Appirater setDaysUntilPrompt:5];
+    [Appirater setUsesUntilPrompt:10];
+    [Appirater setSignificantEventsUntilPrompt:5];
+    [Appirater setTimeBeforeReminding:2];
+    //[Appirater setDebug:YES];
+    
+    [Appirater appLaunched:YES];
+    
     
 }
 

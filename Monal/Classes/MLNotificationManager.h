b139e42923ed0d2ca5b7d3997de03c0eee477153
Anurodh Pokharel <anurodhp@alum.bu.edu>
2013-07-20 20:53:29 -0400

    added  notification manager

---
+++ Monal/Classes/MLNotificationManager.h
@@ -0,0 +1,13 @@
+//
+//  MLNotificationManager.h
+//  Monal
+//
+//  Created by Anurodh Pokharel on 7/20/13.
+//
+//
+
+#import "XMPPParser.h"
+
+@interface MLNotificationManager : XMPPParser
+
+@end
---
+++ Monal/Classes/MLNotificationManager.m
@@ -0,0 +1,16 @@
+//
+//  MLNotificationManager.m
+//  Monal
+//
+//  Created by Anurodh Pokharel on 7/20/13.
+//
+//
+
+#import "MLNotificationManager.h"
+
+/**
+Singleton object that will handle all sliders, alerts and sounds
+ */
+@implementation MLNotificationManager
+
+@end

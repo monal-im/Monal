ebd809b73961f9c2742cd98dc4578f70f7f97350
Anurodh Pokharel <info@monal.im>
2013-08-20 18:31:14 -0400

    fixes #2 crash on about

--- Monal/Classes/AboutViewController.m
+++ Monal/Classes/AboutViewController.m
@@ -42,7 +42,7 @@
     NSError* error = nil;
     NSString *path = [[NSBundle mainBundle] pathForResource: @"About" ofType: @"html"];
     NSString *res = [NSString stringWithContentsOfFile: path encoding:NSUTF8StringEncoding error: &error];
-    NSString* withVer =[NSString stringWithFormat:withVer,
+    NSString* withVer =[NSString stringWithFormat:res,
                          [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"] ,
                          [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"] ];
     

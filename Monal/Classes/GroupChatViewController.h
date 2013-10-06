6077a1e520e7d8c37945f12b91f4954f71872795
Anurodh Pokharel <anurodhp@alum.bu.edu>
2013-10-06 19:16:50 -0400

    completed basic mud selection UI refs #7

--- Monal/Classes/GroupChatViewController.h
+++ Monal/Classes/GroupChatViewController.h
@@ -8,11 +8,21 @@
 
 #import <UIKit/UIKit.h>
 
-@interface GroupChatViewController : UIViewController
+@interface GroupChatViewController : UIViewController <UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate>
+{
+    UIPickerView* _accountPicker;
+    UIView* _accountPickerView;
+    IBOutlet UIToolbar* _keyboardToolbar;
+    UITextField* _currentTextField;
+}
 
 @property (nonatomic, weak) IBOutlet UITextField* roomName; 
-@property (nonatomic, weak) IBOutlet UITextField* serverName;
+@property (nonatomic, weak) IBOutlet UITextField* accountName;
 @property (nonatomic, weak) IBOutlet UITextField* password;
 @property (nonatomic, weak) IBOutlet UIButton* joinButton;
 
+
+- (IBAction)toolbarDone:(id)sender;
+- (IBAction)toolbarPrevious:(id)sender;
+- (IBAction)toolbarNext:(id)sender;
 @end
--- Monal/Classes/GroupChatViewController.m
+++ Monal/Classes/GroupChatViewController.m
@@ -8,6 +8,9 @@
 
 #import "GroupChatViewController.h"
 #import "MLConstants.h"
+#import "TPKeyboardAvoidingScrollView.h"
+
+
 
 @interface GroupChatViewController ()
 
@@ -29,14 +32,31 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     self.navigationItem.title=NSLocalizedString(@"Group Chat",@"");
+    
+    //for time input
+    _accountPicker = [[ UIPickerView alloc] init];
+    _accountPickerView= [[UIView alloc] initWithFrame: _accountPicker.frame];
+    _accountPickerView.autoresizingMask=UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth;
+    
+    
+    [_accountPickerView addSubview:_accountPicker];
+    _accountPicker.delegate=self;
+    _accountPicker.dataSource=self;
+    _accountPicker.autoresizingMask=UIViewAutoresizingFlexibleLeftMargin|UIViewAutoresizingFlexibleRightMargin;
+    
+    self.accountName.inputView=_accountPickerView;
+    
     if(SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0"))
     {
         
     }
     else
     {
+         _accountPickerView.backgroundColor=[UIColor blackColor];
         [self.view setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"debut_dark"]]];
     }
+    
+    
         UIImage *buttonImage = [[UIImage imageNamed:@"blueButton"]
                                  resizableImageWithCapInsets:UIEdgeInsetsMake(18, 18, 18, 18)];
         UIImage *buttonImageHighlight = [[UIImage imageNamed:@"blueButtonHighlight"]
@@ -45,7 +65,112 @@
         [_joinButton setBackgroundImage:buttonImage forState:UIControlStateNormal];
         [_joinButton setBackgroundImage:buttonImageHighlight forState:UIControlStateSelected];
     
+    
+    self.roomName.inputAccessoryView=_keyboardToolbar;
+    self.password.inputAccessoryView=_keyboardToolbar;
+    self.accountName.inputAccessoryView=_keyboardToolbar;
+    
+
+    
 }
+
+#pragma mark picker view delegate
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return @"test";
+}
+
+#pragma mark picker view datasource
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return 1;
+}
+
+
+#pragma mark textField delegate
+
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    
+    
+}
+
+
+-(BOOL)textFieldShouldReturn:(UITextField*)textField;
+{
+    
+    [textField resignFirstResponder];
+    return NO; // We do not want UITextField to insert line-breaks.
+}
+
+//date picker
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    
+    _currentTextField=textField;
+    if(textField==self.accountName)
+    {
+    
+       // return NO;
+    }
+    else
+    {
+    
+      
+    }
+    
+     return YES; 
+}
+
+
+
+#pragma mark toolbar actions
+
+-(IBAction)toolbarDone:(id)sender
+{
+    [_currentTextField resignFirstResponder];
+    
+}
+
+- (IBAction)toolbarPrevious:(id)sender
+{
+    NSInteger nextTag = _currentTextField.tag - 1;
+    // Try to find next responder
+    UIResponder* nextResponder = [_currentTextField.superview viewWithTag:nextTag];
+    if (nextResponder) {
+        // Found next responder, so set it.
+        [nextResponder becomeFirstResponder];
+    } else {
+        // Not found, so remove keyboard.
+        [_currentTextField resignFirstResponder];
+    }
+}
+
+- (IBAction)toolbarNext:(id)sender
+{
+    NSInteger nextTag = _currentTextField.tag + 1;
+    // Try to find next responder
+    UIResponder* nextResponder = [_currentTextField.superview viewWithTag:nextTag];
+    if (nextResponder) {
+        // Found next responder, so set it.
+        [nextResponder becomeFirstResponder];
+    } else {
+        // Not found, so remove keyboard.
+        [_currentTextField resignFirstResponder];
+    }
+}
+
+
 
 - (void)didReceiveMemoryWarning
 {

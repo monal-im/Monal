f89343db46094abb6381dae5499078aad5fcfd14
Anurodh Pokharel <info@monal.im>
2013-12-17 19:10:49 -0500

    contacts not shown in roster unless subscription = both

--- Monal/Classes/xmpp.m
+++ Monal/Classes/xmpp.m
@@ -785,10 +785,17 @@ dispatch_async(dispatch_get_current_queue(), ^{
                     
                     for(NSDictionary* contact in self.rosterList)
                     {
-                
-                        if(![[DataLayer sharedInstance] isBuddyInList:[contact objectForKey:@"jid"] forAccount:_accountNo])
+                        
+                        if([[contact objectForKey:@"subscription"] isEqualToString:@"both"])
                         {
-                        [[DataLayer sharedInstance] addBuddy:[contact objectForKey:@"jid"]?[contact objectForKey:@"jid"]:@"" forAccount:_accountNo fullname:[contact objectForKey:@"name"]?[contact objectForKey:@"name"]:@"" nickname:[contact objectForKey:@"name"]?[contact objectForKey:@"name"]:@""];
+                            if(![[DataLayer sharedInstance] isBuddyInList:[contact objectForKey:@"jid"] forAccount:_accountNo])
+                            {
+                                [[DataLayer sharedInstance] addBuddy:[contact objectForKey:@"jid"]?[contact objectForKey:@"jid"]:@"" forAccount:_accountNo fullname:[contact objectForKey:@"name"]?[contact objectForKey:@"name"]:@"" nickname:[contact objectForKey:@"name"]?[contact objectForKey:@"name"]:@""];
+                            }
+                        }
+                        else
+                        {
+                            
                         }
                     }
                     

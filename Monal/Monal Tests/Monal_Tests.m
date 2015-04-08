8bfae05834fa124b146a83167cb41310ceb6a03c
Anurodh Pokharel <info@monal.im>
2015-04-08 08:04:30 -0400

    added unit test

---
+++ Monal/Monal Tests/Monal_Tests.m
@@ -0,0 +1,40 @@
+//
+//  Monal_Tests.m
+//  Monal Tests
+//
+//  Created by Anurodh Pokharel on 4/8/15.
+//  Copyright (c) 2015 Monal.im. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Monal_Tests : XCTestCase
+
+@end
+
+@implementation Monal_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

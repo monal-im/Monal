3f01bc52fe9b07f21fc3df25f08942ef8ecd0e69
Anurodh Pokharel <anurodhp@alum.bu.edu>
2015-06-28 10:22:35 -0400

    added toolbar  and plitview delegate

---
+++ Monal/Monal-OSX/MLSplitViewDelegate.h
@@ -0,0 +1,14 @@
+//
+//  MLSplitViewDelegate.h
+//  Monal
+//
+//  Created by Anurodh Pokharel on 6/28/15.
+//  Copyright (c) 2015 Monal.im. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+
+@interface MLSplitViewDelegate : NSObject <NSSplitViewDelegate>
+
+@end
---
+++ Monal/Monal-OSX/MLSplitViewDelegate.m
@@ -0,0 +1,18 @@
+//
+//  MLSplitViewDelegate.m
+//  Monal
+//
+//  Created by Anurodh Pokharel on 6/28/15.
+//  Copyright (c) 2015 Monal.im. All rights reserved.
+//
+
+#import "MLSplitViewDelegate.h"
+
+@implementation MLSplitViewDelegate
+
+- (BOOL)splitView:(NSSplitView *)splitView shouldAdjustSizeOfSubview:(NSView *)subview
+{
+    return YES;
+}
+
+@end

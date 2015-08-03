37304798fd470f82a36b7c1a503c7113092b2bc5
Anurodh Pokharel <info@monal.im>
2015-08-03 19:44:15 -0400

    added account edit

---
+++ Monal/Monal-OSX/MLAccountEdit.h
@@ -0,0 +1,13 @@
+//
+//  MLAccountEdit.h
+//  Monal
+//
+//  Created by Anurodh Pokharel on 8/3/15.
+//  Copyright (c) 2015 Monal.im. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MLAccountEdit : NSViewController
+
+@end
---
+++ Monal/Monal-OSX/MLAccountEdit.m
@@ -0,0 +1,22 @@
+//
+//  MLAccountEdit.m
+//  Monal
+//
+//  Created by Anurodh Pokharel on 8/3/15.
+//  Copyright (c) 2015 Monal.im. All rights reserved.
+//
+
+#import "MLAccountEdit.h"
+
+@interface MLAccountEdit ()
+
+@end
+
+@implementation MLAccountEdit
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end

65f31ef33d2a4ec92a2b7123219900f6e56d618b
Anurodh Pokharel <info@monal.im>
2015-11-19 00:32:02 -0500

    show account names

--- Monal/Monal-OSX/MLAddContact.m
+++ Monal/Monal-OSX/MLAddContact.m
@@ -7,8 +7,11 @@
 //
 
 #import "MLAddContact.h"
+#import "MLXMPPManager.h"
+#import "DataLayer.h"
 
 @interface MLAddContact ()
+@property (nonatomic, strong) NSMutableArray *displyedAccounts;
 
 @end
 
@@ -19,6 +22,22 @@
     // Do view setup here.
 }
 
+
+-(void) viewWillAppear
+{
+    NSArray *accountsArray = [[DataLayer sharedInstance] accountList];
+    self.displyedAccounts= [[NSMutableArray alloc] init];
+    
+    for (NSDictionary *account in accountsArray)
+    {
+        if([[MLXMPPManager sharedInstance] isAccountForIdConnected:[NSString stringWithFormat:@"%@", [account objectForKey:@"account_id"]]])
+        {
+            [self.accounts addItemWithObjectValue:[NSString stringWithFormat:@"%@@%@", [account objectForKey:@"account_name"], [account objectForKey:@"domain"]]];
+            [self.displyedAccounts addObject:account];
+        }
+    }
+    
+}
 
 -(IBAction)add:(id)sender
 {

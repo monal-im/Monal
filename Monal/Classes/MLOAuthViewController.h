395734ed827f8940346b4bccd164b5b0fa1e7c73
Anurodh Pokharel <info@monal.im>
2016-01-16 07:24:40 -0500

    #359 began adding google 2 factor

---
+++ Monal/Classes/MLOAuthViewController.h
@@ -0,0 +1,13 @@
+//
+//  MLOAuthViewController.h
+//  Monal
+//
+//  Created by Anurodh Pokharel on 1/16/16.
+//  Copyright © 2016 Monal.im. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MLOAuthViewController : NSViewController
+
+@end
---
+++ Monal/Classes/MLOAuthViewController.m
@@ -0,0 +1,13 @@
+//
+//  MLOAuthViewController.m
+//  Monal
+//
+//  Created by Anurodh Pokharel on 1/16/16.
+//  Copyright © 2016 Monal.im. All rights reserved.
+//
+
+#import "MLOAuthViewController.h"
+
+@implementation MLOAuthViewController
+
+@end

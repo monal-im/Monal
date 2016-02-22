b75910988c632df0f81fe5a12fc1d1de540b9b2a
Anurodh Pokharel <info@monal.im>
2016-02-21 21:18:09 -0500

    fixed content inset

--- Monal/Classes/chatViewController.m
+++ Monal/Classes/chatViewController.m
@@ -165,6 +165,9 @@ static const int ddLogLevel = LOG_LEVEL_ERROR;
         
     }
 
+    UIEdgeInsets currentInset = self.messageTable.contentInset;
+    self.messageTable.contentInset =UIEdgeInsetsMake(self.navigationController.navigationBar.frame.size.height+[UIApplication sharedApplication].statusBarFrame.size.height, currentInset.left, currentInset.bottom, currentInset.right);
+    
 }
 
 -(void) viewDidAppear:(BOOL)animated

fd8401f82517340d0d5664491d22a5daefe6f49c
Anurodh Pokharel <info@monal.im>
2017-04-16 16:49:33 -0400

    prevent crashing with a nil insert

--- Monal/Classes/xmpp.m
+++ Monal/Classes/xmpp.m
@@ -1632,7 +1632,7 @@ static const int ddLogLevel = LOG_LEVEL_VERBOSE;
                                                                       NSDictionary* userDic=@{@"from":messageNode.from,
                                                                                               @"actuallyfrom":actuallyFrom,
                                                                                               @"messageText":messageText,
-                                                                                              @"to":messageNode.to,
+                                                                                              @"to":messageNode.to?messageNode.to:recipient,
                                                                                               @"accountNo":_accountNo,
                                                                                               @"showAlert":[NSNumber numberWithBool:showAlert],
                                                                                               @"shouldRefresh":[NSNumber numberWithBool:shouldRefresh]

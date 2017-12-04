9ba09ce3b1931738f76037572739369b46f11c88
Anurodh Pokharel <info@monal.im>
2017-12-04 11:51:02 -0500

    collapse secondary view controller

---
+++ Monal/Classes/MLSplitViewDelegate.h
@@ -0,0 +1,13 @@
+//
+//  MLSplitViewDelegate.h
+//  Monal
+//
+//  Created by Anurodh Pokharel on 12/4/17.
+//  Copyright © 2017 Monal.im. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MLSplitViewDelegate : NSObject <UISplitViewControllerDelegate>
+
+@end

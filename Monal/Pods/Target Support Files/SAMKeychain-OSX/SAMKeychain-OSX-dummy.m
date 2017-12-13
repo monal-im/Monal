fb716a91c58756139a327b422a4ee46f1dc65e46
Anurodh Pokharel <anurodhp@alum.bu.edu>
2017-12-13 08:45:32 -0500

    updated pods  and icons

--- Monal/Pods/Target Support Files/SAMKeychain-OSX/SAMKeychain-OSX-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SAMKeychain_OSX : NSObject
-@end
-@implementation PodsDummy_SAMKeychain_OSX
-@end
---
+++ Monal/Pods/Target Support Files/SAMKeychain-macOS/SAMKeychain-macOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SAMKeychain_macOS : NSObject
+@end
+@implementation PodsDummy_SAMKeychain_macOS
+@end

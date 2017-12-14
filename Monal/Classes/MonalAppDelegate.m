9545c538c6e54133c8db1775ee8ac9803b8e9d82
Anurodh Pokharel <anurodhp@alum.bu.edu>
2017-12-14 17:19:31 -0500

    correct push address

--- Monal/Classes/MonalAppDelegate.m
+++ Monal/Classes/MonalAppDelegate.m
@@ -99,7 +99,7 @@ static const int ddLogLevel = LOG_LEVEL_VERBOSE;
     
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
     //this is the hardcoded push api endpoint
-    [request setURL:[NSURL URLWithString:@"http://192.168.2.3:5280/push_appserver/v1/register"]];
+    [request setURL:[NSURL URLWithString:@"https://push.monal.im:5281/push_appserver/v1/register"]];
     [request setHTTPMethod:@"POST"];
     [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
     [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];

fe1ad861755e05ad55ba785e146414704b65c05c
Anurodh Pokharel <anurodhp@alum.bu.edu>
2018-03-25 09:15:52 -0400

    group chat view controllers for iOS

---
+++ Monal/Classes/MLGroupChatTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MLGroupChatTableViewController.h
+//  Monal
+//
+//  Created by Anurodh Pokharel on 3/25/18.
+//  Copyright © 2018 Monal.im. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLGroupChatTableViewController : UITableViewController
+
+@end
---
+++ Monal/Classes/MLRoomListViewController.h
@@ -0,0 +1,13 @@
+//
+//  MLRoomListViewController.h
+//  Monal
+//
+//  Created by Anurodh Pokharel on 3/25/18.
+//  Copyright © 2018 Monal.im. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLRoomListViewController : UITableViewController
+
+@end

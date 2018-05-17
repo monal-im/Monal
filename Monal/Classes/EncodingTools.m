d6a27483b2f007d9ca8ff30964aafb2251fe731f
Anurodh Pokharel <anurodhp@alum.bu.edu>
2018-05-17 18:55:36 -0400

    updated pods and changed bas64 handling

--- Monal/Classes/EncodingTools.m
+++ Monal/Classes/EncodingTools.m
@@ -15,147 +15,17 @@
 #pragma mark  Bae64
 
 
-static const char _base64EncodingTable[64] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-static const short _base64DecodingTable[256] = {
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -1, -1, -2, -1, -1, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -1, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, 62, -2, -2, -2, 63,
-    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -2, -2, -2, -2, -2, -2,
-    -2,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
-    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -2, -2, -2, -2, -2,
-    -2, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2
-};
-
-
 + (NSString *)encodeBase64WithString:(NSString *)strData {
-    return [EncodingTools encodeBase64WithData:[strData dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    NSData *data =[strData dataUsingEncoding:NSUTF8StringEncoding];
+    
+    return [data base64EncodedStringWithOptions:NSDataBase64EncodingEndLineWithLineFeed];
+    
 }
-
-+ (NSString *)encodeBase64WithData:(NSData *)objData {
-    const unsigned char * objRawData = [objData bytes];
-    char * objPointer;
-    char * strResult;
-    
-    // Get the Raw Data length and ensure we actually have data
-    NSUInteger intLength = [objData length];
-    if (intLength == 0) return nil;
-    
-    // Setup the String-based Result placeholder and pointer within that placeholder
-    strResult = (char *)calloc((((intLength + 2) / 3) * 4) + 1, sizeof(char));
-    objPointer = strResult;
-    
-    // Iterate through everything
-    while (intLength > 2) { // keep going until we have less than 24 bits
-        *objPointer++ = _base64EncodingTable[objRawData[0] >> 2];
-        *objPointer++ = _base64EncodingTable[((objRawData[0] & 0x03) << 4) + (objRawData[1] >> 4)];
-        *objPointer++ = _base64EncodingTable[((objRawData[1] & 0x0f) << 2) + (objRawData[2] >> 6)];
-        *objPointer++ = _base64EncodingTable[objRawData[2] & 0x3f];
-        
-        // we just handled 3 octets (24 bits) of data
-        objRawData += 3;
-        intLength -= 3;
-    }
-    
-    // now deal with the tail end of things
-    if (intLength != 0) {
-        *objPointer++ = _base64EncodingTable[objRawData[0] >> 2];
-        if (intLength > 1) {
-            *objPointer++ = _base64EncodingTable[((objRawData[0] & 0x03) << 4) + (objRawData[1] >> 4)];
-            *objPointer++ = _base64EncodingTable[(objRawData[1] & 0x0f) << 2];
-            *objPointer++ = '=';
-        } else {
-            *objPointer++ = _base64EncodingTable[(objRawData[0] & 0x03) << 4];
-            *objPointer++ = '=';
-            *objPointer++ = '=';
-        }
-    }
-    
-    // Terminate the string-based result
-    *objPointer = '\0';
-    
-    // Return the results as an NSString object
-    NSString* toReturn= [NSString stringWithCString:strResult encoding:NSASCIIStringEncoding];
-    free(strResult);
-    
-    return toReturn;
-}
-
 
 + (NSData*) dataWithBase64EncodedString:(NSString *)string
 {
-	char encodingTable[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-	
-	if (string == nil)
-		[NSException raise:NSInvalidArgumentException format:nil];
-	if ([string length] == 0)
-		return [NSData data];
-	
-	static char *decodingTable = NULL;
-	if (decodingTable == NULL)
-	{
-		decodingTable = malloc(256);
-		if (decodingTable == NULL)
-			return nil;
-		memset(decodingTable, CHAR_MAX, 256);
-		NSUInteger i;
-		for (i = 0; i < 64; i++)
-			decodingTable[(short)encodingTable[i]] = i;
-	}
-	
-	const char *characters = [string cStringUsingEncoding:NSASCIIStringEncoding];
-	if (characters == NULL)     //  Not an ASCII string!
-		return nil;
-	char *bytes = malloc((([string length] + 3) / 4) * 3);
-	if (bytes == NULL)
-		return nil;
-	NSUInteger length = 0;
-	
-	NSUInteger i = 0;
-	while (YES)
-	{
-		char buffer[4];
-		short bufferLength;
-		for (bufferLength = 0; bufferLength < 4; i++)
-		{
-			if (characters[i] == '\0')
-				break;
-			if (isspace(characters[i]) || characters[i] == '=')
-				continue;
-			buffer[bufferLength] = decodingTable[(short)characters[i]];
-			if (buffer[bufferLength++] == CHAR_MAX)      //  Illegal character!
-			{
-				free(bytes);
-				return nil;
-			}
-		}
-		
-		if (bufferLength == 0)
-			break;
-		if (bufferLength == 1)      //  At least two characters are needed to produce one byte!
-		{
-			free(bytes);
-			return nil;
-		}
-		
-		//  Decode the characters in the buffer to bytes.
-		bytes[length++] = (buffer[0] << 2) | (buffer[1] >> 4);
-		if (bufferLength > 2)
-			bytes[length++] = (buffer[1] << 4) | (buffer[2] >> 2);
-		if (bufferLength > 3)
-			bytes[length++] = (buffer[2] << 6) | buffer[3];
-	}
-	
-	realloc(bytes, length);
-	return [NSData dataWithBytesNoCopy:bytes length:length];
+    return [[NSData alloc] initWithBase64EncodedString:string options:NSDataBase64DecodingIgnoreUnknownCharacters];
 }
 
 
--- Monal/Pods/Target Support Files/Pods-Monal/Pods-Monal-acknowledgements.markdown
+++ Monal/Pods/Target Support Files/Pods-Monal/Pods-Monal-acknowledgements.markdown
@@ -28,7 +28,7 @@ THE SOFTWARE.
 
 ## Crashlytics
 
-Fabric: Copyright 2017 Google, Inc. All Rights Reserved. Use of this software is subject to the terms and conditions of the Fabric Software and Services Agreement located at https://fabric.io/terms. Crashlytics Kit: Copyright 2017 Crashlytics, Inc. All Rights Reserved. Use of this software is subject to the terms and conditions of the Crashlytics Terms of Service located at http://try.crashlytics.com/terms/terms-of-service.pdf and the Crashlytics Privacy Policy located at http://try.crashlytics.com/terms/privacy-policy.pdf. OSS: http://get.fabric.io/terms/opensource.txt
+Fabric: Copyright 2018 Google, Inc. All Rights Reserved. Use of this software is subject to the terms and conditions of the Fabric Software and Services Agreement located at https://fabric.io/terms. Crashlytics Kit: Copyright 2018 Crashlytics, Inc. All Rights Reserved. Use of this software is subject to the terms and conditions of the Crashlytics Terms of Service located at http://try.crashlytics.com/terms/terms-of-service.pdf and the Crashlytics Privacy Policy located at http://try.crashlytics.com/terms/privacy-policy.pdf. OSS: http://get.fabric.io/terms/opensource.txt
 
 ## DACircularProgress
 
@@ -82,7 +82,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 ## Fabric
 
-Fabric: Copyright 2017 Google, Inc. All Rights Reserved. Use of this software is subject to the terms and conditions of the Fabric Software and Services Agreement located at https://fabric.io/terms. OSS: http://get.fabric.io/terms/opensource.txt
+Fabric: Copyright 2018 Google, Inc. All Rights Reserved. Use of this software is subject to the terms and conditions of the Fabric Software and Services Agreement located at https://fabric.io/terms. OSS: http://get.fabric.io/terms/opensource.txt
 
 ## MBProgressHUD
 

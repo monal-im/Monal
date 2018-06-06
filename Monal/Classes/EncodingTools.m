10015d1c69bfac42e54dbdad1df74a4118191b89
Anurodh Pokharel <anurodhp@alum.bu.edu>
2018-06-05 20:00:17 -0400

    use  base64 decoding

--- Monal/Classes/EncodingTools.m
+++ Monal/Classes/EncodingTools.m
@@ -16,24 +16,6 @@
 
 
 static const char _base64EncodingTable[64] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-static const short _base64DecodingTable[256] = {
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -1, -1, -2, -1, -1, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -1, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, 62, -2, -2, -2, 63,
-    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -2, -2, -2, -2, -2, -2,
-    -2,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
-    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -2, -2, -2, -2, -2,
-    -2, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2
-};
 
 
 + (NSString *)encodeBase64WithString:(NSString *)strData {
@@ -92,70 +74,8 @@ static const short _base64DecodingTable[256] = {
 
 + (NSData*) dataWithBase64EncodedString:(NSString *)string
 {
-	char encodingTable[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-	
-	if (string == nil)
-		[NSException raise:NSInvalidArgumentException format:nil];
-	if ([string length] == 0)
-		return [NSData data];
-	
-	static char *decodingTable = NULL;
-	if (decodingTable == NULL)
-	{
-		decodingTable = malloc(256);
-		if (decodingTable == NULL)
-			return nil;
-		memset(decodingTable, CHAR_MAX, 256);
-		NSUInteger i;
-		for (i = 0; i < 64; i++)
-			decodingTable[(short)encodingTable[i]] = i;
-	}
-	
-	const char *characters = [string cStringUsingEncoding:NSASCIIStringEncoding];
-	if (characters == NULL)     //  Not an ASCII string!
-		return nil;
-	char *bytes = malloc((([string length] + 3) / 4) * 3);
-	if (bytes == NULL)
-		return nil;
-	NSUInteger length = 0;
-	
-	NSUInteger i = 0;
-	while (YES)
-	{
-		char buffer[4];
-		short bufferLength;
-		for (bufferLength = 0; bufferLength < 4; i++)
-		{
-			if (characters[i] == '\0')
-				break;
-			if (isspace(characters[i]) || characters[i] == '=')
-				continue;
-			buffer[bufferLength] = decodingTable[(short)characters[i]];
-			if (buffer[bufferLength++] == CHAR_MAX)      //  Illegal character!
-			{
-				free(bytes);
-				return nil;
-			}
-		}
-		
-		if (bufferLength == 0)
-			break;
-		if (bufferLength == 1)      //  At least two characters are needed to produce one byte!
-		{
-			free(bytes);
-			return nil;
-		}
-		
-		//  Decode the characters in the buffer to bytes.
-		bytes[length++] = (buffer[0] << 2) | (buffer[1] >> 4);
-		if (bufferLength > 2)
-			bytes[length++] = (buffer[1] << 4) | (buffer[2] >> 2);
-		if (bufferLength > 3)
-			bytes[length++] = (buffer[2] << 6) | buffer[3];
-	}
-	
-	realloc(bytes, length);
-	return [NSData dataWithBytesNoCopy:bytes length:length];
+    NSData *toReturn = [[NSData alloc] initWithBase64EncodedString:string options:NSDataBase64DecodingIgnoreUnknownCharacters];
+    return toReturn;
 }
 
 

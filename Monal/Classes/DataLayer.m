88715007ddb0f7b6bb010217938b999e30043fee
Anurodh Pokharel <anurodhp@alum.bu.edu>
2018-06-22 08:49:52 -0400

    fixed queries

--- Monal/Classes/DataLayer.m
+++ Monal/Classes/DataLayer.m
@@ -2376,7 +2376,7 @@ static DataLayer *sharedInstance=nil;
     {
         DDLogVerbose(@"Database version <3.0 detected. Performing upgrade . ");
         
-        [self executeNonQuery:@"CREATE TABLE signalIdentity (deviceid int NOT NULL PRIMARY KEY, account_id int NOT NULL,identityPublicKey BLOB,identityPrivateKey BLOB)" withCompletion:nil];
+        [self executeNonQuery:@"CREATE TABLE signalIdentity (deviceid int NOT NULL PRIMARY KEY, account_id int NOT NULL unique,identityPublicKey BLOB,identityPrivateKey BLOB)" withCompletion:nil];
         [self executeNonQuery:@"CREATE TABLE signalSignedPreKey (account_id int NOT NULL,signedPreKeyId int not null,signedPreKey BLOB);" withCompletion:nil];
         
         [self executeNonQuery:@"CREATE TABLE signalPreKey (account_id int NOT NULL,prekeyid int not null,preKey BLOB);" withCompletion:nil];

ba05538d8476c9a7ee9e7eb9c7db257484a5ab71
Anurodh Pokharel <info@monal.im>
2018-09-10 12:33:22 -0400

    ios share sheet

---
+++ Monal/shareSheet-iOS/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  shareSheet
+//
+//  Created by Anurodh Pokharel on 9/10/18.
+//  Copyright © 2018 Monal.im. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
---
+++ Monal/shareSheet-iOS/ShareViewController.m
@@ -0,0 +1,41 @@
+//
+//  ShareViewController.m
+//  shareSheet
+//
+//  Created by Anurodh Pokharel on 9/10/18.
+//  Copyright © 2018 Monal.im. All rights reserved.
+//
+
+#import "ShareViewController.h"
+
+@interface ShareViewController ()
+
+@end
+
+@implementation ShareViewController
+
+- (BOOL)isContentValid {
+    // Do validation of contentText and/or NSExtensionContext attachments here
+    return YES;
+}
+
+- (void)didSelectPost {
+    // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+    
+    // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+    [self.extensionContext completeRequestReturningItems:@[] completionHandler:nil];
+}
+
+- (NSArray *)configurationItems {
+    // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+    SLComposeSheetConfigurationItem *account = [[SLComposeSheetConfigurationItem alloc] init];
+    account.title=@"Account";
+    account.value=@"anurodhp@jabb3r.org";
+    SLComposeSheetConfigurationItem *recipient = [[SLComposeSheetConfigurationItem alloc] init];
+    recipient.title=@"Recipient";
+    recipient.value=@"monal2@jabb3r.org";
+    
+    return @[account, recipient];
+}
+
+@end

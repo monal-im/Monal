3444142f3229700067fa6514170ec4404a4b8e39
Anurodh Pokharel <info@monal.im>
2018-10-25 09:55:14 -0400

    ios share saves url

--- Monal/shareSheet-iOS/ShareViewController.m
+++ Monal/shareSheet-iOS/ShareViewController.m
@@ -7,12 +7,18 @@
 //
 
 #import "ShareViewController.h"
+@import Crashlytics;
+@import Fabric;
 
 @interface ShareViewController ()
 
 @end
 
 @implementation ShareViewController
+
+- (void)presentationAnimationDidFinish {
+      [Fabric with:@[[Crashlytics class]]];
+}
 
 - (BOOL)isContentValid {
     // Do validation of contentText and/or NSExtensionContext attachments here
@@ -23,10 +29,45 @@
     // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
     
     //get text
-   // self.contentText;
-    [[NSURLSession sharedSession] dataTaskWithURL:[NSURL URLWithString:@"https://monal.im/wakeios/"]]; 
+    NSMutableDictionary *payload = [[NSMutableDictionary alloc] init];
+    
+    NSExtensionItem *item = self.extensionContext.inputItems.firstObject;
+    NSLog(@"Attachments = %@", item.attachments);
+    
+    
+    for (NSItemProvider *provider in item.attachments)
+    {
+       if([provider hasItemConformingToTypeIdentifier:@"public.url"])
+       {
+           [provider loadItemForTypeIdentifier:@"public.url" options:NULL completionHandler:^(NSURL<NSSecureCoding>*  _Nullable item, NSError * _Null_unspecified error) {
+                [payload setObject:item.absoluteString forKey:@"url"];
+                [payload setObject:self.contentText forKey:@"comment"];
+               
+               NSUserDefaults *groupDefaults= [[NSUserDefaults alloc] initWithSuiteName:@"group.monal"];
+               NSMutableArray *outbox=[[groupDefaults objectForKey:@"outbox"] mutableCopy];
+               if(!outbox) outbox =[[NSMutableArray alloc] init];
+               
+               [outbox addObject:payload];
+               [groupDefaults setObject:outbox forKey:@"outbox"];
+               [groupDefaults synchronize];
+               
+           }];
+       }
+    }
+    
+   
+
+    
+    
+//    [[[NSURLSession sharedSession] dataTaskWithURL:[NSURL URLWithString:@"https://monal.im/wakeios"] completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+//
+//
+//    }] resume];
+    
+    
+    
     // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-    [self.extensionContext completeRequestReturningItems:@[] completionHandler:nil];
+     [self.extensionContext completeRequestReturningItems:@[] completionHandler:nil];
 }
 
 - (NSArray *)configurationItems {

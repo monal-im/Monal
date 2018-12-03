75038cb3091184d6a50042e452c44db5d26da8c9
Anurodh Pokharel <info@monal.im>
2018-12-02 22:56:35 -0500

    compose button, position change

--- Monal/Classes/MLComposeViewController.h
+++ Monal/Classes/MLComposeViewController.h
@@ -10,9 +10,23 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface MLComposeViewController : UITableViewController
+@interface MLComposeViewController : UITableViewController <UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate> {
+    UITextField* _currentTextField;
+    UIPickerView* _accountPicker;
+    UIView* _accountPickerView;
+    NSInteger _selectedRow;
+}
+
+@property (nonatomic, weak)  UITextField* contactName;
+@property (nonatomic, weak)  UITextField* accountName;
+@property (nonatomic, weak) IBOutlet UIToolbar* keyboardToolbar;
 
 - (IBAction)close:(id)sender;
+
+- (IBAction)toolbarDone:(id)sender;
+- (IBAction)toolbarPrevious:(id)sender;
+- (IBAction)toolbarNext:(id)sender;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- Monal/Classes/MLComposeViewController.m
+++ Monal/Classes/MLComposeViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "MLComposeViewController.h"
+#import "MLConstants.h"
+#import "MLXMPPManager.h"
+#import "MLButtonCell.h"
+#import "MLTextInputCell.h"
 
 @interface MLComposeViewController ()
 
@@ -14,90 +18,225 @@
 
 @implementation MLComposeViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
-
--(void) viewWillAppear:(BOOL)animated
-{
-    
-}
-
 - (IBAction)close:(id)sender
 {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
-#pragma mark - Table view data source
+#pragma mark - textfield delegate
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
-}
-
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
     
-    // Configure the cell...
+    return YES;
+}
+
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    
+    _currentTextField=textField;
+    return YES;
+}
+
+
+#pragma mark View life cycle
+
+-(void) viewDidLoad
+{
+    [super viewDidLoad];
+    self.navigationItem.title=@"Send A Message";
+    
+    _accountPicker = [[ UIPickerView alloc] init];
+    _accountPickerView= [[UIView alloc] initWithFrame: _accountPicker.frame];
+    _accountPickerView.autoresizingMask=UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth;
+    
+    
+    [_accountPickerView addSubview:_accountPicker];
+    _accountPicker.delegate=self;
+    _accountPicker.dataSource=self;
+    _accountPicker.autoresizingMask=UIViewAutoresizingFlexibleLeftMargin|UIViewAutoresizingFlexibleRightMargin;
+    
+    
+    [self.tableView registerNib:[UINib nibWithNibName:@"MLTextInputCell"
+                                               bundle:[NSBundle mainBundle]]
+         forCellReuseIdentifier:@"TextCell"];
+    
+    
+}
+
+-(void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [_accountPicker reloadAllComponents];
+    
+    if([[MLXMPPManager sharedInstance].connectedXMPP count]==1)
+    {
+        [[MLXMPPManager sharedInstance] getServiceDetailsForAccount:0 ];
+        [_accountPicker selectedRowInComponent:0];
+        
+    }
+}
+
+#pragma mark tableview datasource delegate
+
+-(NSInteger) numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 2;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    if(section==0)
+    {
+        return @"Contacts are usually in the format: username@domain.something";
+    }
+    else return nil;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    NSInteger toreturn =0;
+    switch (section) {
+        case 0:
+            toreturn =2;
+            break;
+        case 1:
+            toreturn=1;
+            break;
+            
+        default:
+            break;
+    }
+    
+    return toreturn;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell ;
+    
+    switch (indexPath.section) {
+        case 0: {
+            MLTextInputCell *textCell =[tableView dequeueReusableCellWithIdentifier:@"TextCell"];
+            if(indexPath.row ==0){
+                
+                if([[MLXMPPManager sharedInstance].connectedXMPP count]==1)
+                {
+                    self.accountName.text=[[MLXMPPManager sharedInstance] getAccountNameForConnectedRow:0];
+                }
+                
+                if([[MLXMPPManager sharedInstance].connectedXMPP count]>1){
+                    self.accountName =textCell.textInput;
+                    self.accountName.placeholder = @"Account";
+                    self.accountName.inputView=_accountPickerView;
+                    self.accountName.delegate=self;
+                } else  {
+                    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"blank"];
+                    cell.contentView.backgroundColor= [UIColor groupTableViewBackgroundColor];
+                    break;
+                }
+                
+            }
+            else   if(indexPath.row ==1){
+                self.contactName =textCell.textInput;
+                self.contactName.placeholder = @"Contact Name";
+                self.contactName.delegate=self;
+            }
+            textCell.textInput.inputAccessoryView =_keyboardToolbar;
+            
+            cell= textCell;
+            break;
+        }
+        case 1: {
+            
+            cell =[tableView dequeueReusableCellWithIdentifier:@"addButton"];
+            
+            
+            break;
+        }
+        default:
+            break;
+    }
     
     return cell;
+    
 }
-*/
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+#pragma mark tableview delegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
 }
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+
+#pragma mark picker view delegate
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    _selectedRow=row;
+    _accountName.text=[[MLXMPPManager sharedInstance] getAccountNameForConnectedRow:row];
+    
+    [[MLXMPPManager sharedInstance] getServiceDetailsForAccount:row ];
+    
 }
-*/
 
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    if(row< [[MLXMPPManager sharedInstance].connectedXMPP count])
+    {
+        NSString* name =[[MLXMPPManager sharedInstance] getAccountNameForConnectedRow:row];
+        if(name)
+            return name;
+    }
+    return @"Unnamed";
 }
-*/
 
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
+#pragma mark picker view datasource
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
 }
-*/
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return [[MLXMPPManager sharedInstance].connectedXMPP count];
 }
-*/
+
+#pragma mark toolbar actions
+
+-(IBAction)toolbarDone:(id)sender
+{
+    if(_currentTextField ==self.contactName)
+    {
+        [self.contactName resignFirstResponder];
+    }
+    else {
+        [self.accountName resignFirstResponder];
+    }
+    
+}
+
+- (IBAction)toolbarPrevious:(id)sender
+{
+    if(_currentTextField ==self.contactName)
+    {
+        [self.accountName becomeFirstResponder];
+    }
+    else {
+        [self.contactName becomeFirstResponder];
+    }
+}
+
+- (IBAction)toolbarNext:(id)sender
+{
+    if(_currentTextField ==self.contactName)
+    {
+        [self.accountName becomeFirstResponder];
+    }
+    else {
+        [self.contactName becomeFirstResponder];
+    }
+}
+
 
 @end

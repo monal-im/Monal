3532a31c0e66637e35fe50a8cddf926bc999b32d
Anurodh Pokharel <info@monal.im>
2018-12-22 11:02:18 -0500

    began notificaiton refactor

--- Monal/Classes/MLNotificationManager.m
+++ Monal/Classes/MLNotificationManager.m
@@ -43,110 +43,121 @@ static const int ddLogLevel = LOG_LEVEL_VERBOSE;
             if ([[notification.userInfo objectForKey:@"showAlert"] boolValue]) {
                 dispatch_async(dispatch_get_main_queue(),
                                ^{
-                                   NSString* acctString =[NSString stringWithFormat:@"%ld", (long)[[notification.userInfo objectForKey:@"accountNo"] integerValue]];  
-                                   NSString* fullName =[[DataLayer sharedInstance] fullName:[notification.userInfo objectForKey:@"from"] forAccount:acctString];
-                                   
-                                   NSString* nameToShow=[notification.userInfo objectForKey:@"from"];
-                                   if([fullName length]>0) nameToShow=fullName;
-                                   
-                                   if(([UIApplication sharedApplication].applicationState==UIApplicationStateBackground)
-                                      || ([UIApplication sharedApplication].applicationState==UIApplicationStateInactive ))
-                                   {
-                                       //present notification
-                                       
-                                       NSDate* theDate=[NSDate dateWithTimeIntervalSinceNow:0]; //immediate fire
-                                       
-                                       UIApplication* app = [UIApplication sharedApplication];
-                                       NSArray*    oldNotifications = [app scheduledLocalNotifications];
-                                       
-                                       // Clear out the old notification before scheduling a new one.
-                                       if ([oldNotifications count] > 0)
-                                           [app cancelAllLocalNotifications];
-                                       
-                                       // Create a new notification
-                                       UILocalNotification* alarm = [[UILocalNotification alloc] init];
-                                       if (alarm)
-                                       {
-                                           //scehdule info
-                                           alarm.fireDate = theDate;
-                                           alarm.timeZone = [NSTimeZone defaultTimeZone];
-                                           alarm.repeatInterval = 0;
-                                           alarm.category=@"Reply";
-                                           
-                                           
-                                           if([[NSUserDefaults standardUserDefaults] boolForKey:@"MessagePreview"]) {
-                                               alarm.alertTitle  =nameToShow;
-                                               alarm.alertBody =[notification.userInfo objectForKey:@"messageText"];
-                                           }  else {
-                                               alarm.alertTitle =  nameToShow;
-                                           }
-                                           
-                                           if( [[NSUserDefaults standardUserDefaults] boolForKey:@"Sound"]==true)
-                                           {
-                                               NSString *filename = [[NSUserDefaults standardUserDefaults] objectForKey:@"AlertSoundFile"];
-                                               if(filename) {
-                                                   alarm.soundName=[NSString stringWithFormat:@"AlertSounds/%@.aif",filename];
-                                               } else  {
-                                                   alarm.soundName=UILocalNotificationDefaultSoundName;
-                                               }
-                                           }
-                                           
-                                           alarm.userInfo=notification.userInfo;
-                                           [app scheduleLocalNotification:alarm];
-                                           
-                                           DDLogVerbose(@"Scheduled local message alert "); 
-                                           
-                                       }
-                                       
-                                       
-                                   }
-                                   else
-                                   {
-                                       
-                                       if(!([[notification.userInfo objectForKey:@"from"] isEqualToString:self.currentContact]) &&
-                                          !([[notification.userInfo objectForKey:@"to"] isEqualToString:self.currentContact] ) )
-                                           //  &&![[notification.userInfo objectForKey:@"from"] isEqualToString:@"Info"]
-                                           
-                                       {
-                                           
-                                           
-                                           if(SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"10.0")){
-                                               
-                                               UNMutableNotificationContent* content = [[UNMutableNotificationContent alloc] init];
-                                               content.title =[notification.userInfo objectForKey:@"from"];
-                                               content.body =[notification.userInfo objectForKey:@"messageText"];
-                                               content.sound = [UNNotificationSound defaultSound];
-                                               content.userInfo= notification.userInfo;
-                                               
-                                               UNNotificationRequest* request = [UNNotificationRequest requestWithIdentifier:[NSUUID alloc].UUIDString
-                                                                                                                     content:content trigger:nil];
-                                               
-                                               UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
-                                               
-                                               [center addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
-                                                   
-                                               }];
-                                               
-                                               
-                                           } else  {
-                                               
-                                               
-                                               SlidingMessageViewController* slidingView= [[SlidingMessageViewController alloc] correctSliderWithTitle:nameToShow message:[notification.userInfo objectForKey:@"messageText"] user:[notification.userInfo objectForKey:@"from"] account:[notification.userInfo objectForKey:@"accountNo"] ];
-                                               
-                                               [self.window addSubview:slidingView.view];
-                                               
-                                               [slidingView showMsg];
-                                           }
-                                           
-                                       }
-                                       
-                                   }
-                                   
+                                   [self presentAlert:notification];
                                });
             }
         }
     }];
 }
+
+/**
+ for io10 and up
+ */
+-(void) showModernNotificaion:(NSNotification *)notification
+{
+    UNMutableNotificationContent* content = [[UNMutableNotificationContent alloc] init];
+    content.title =[notification.userInfo objectForKey:@"from"];
+    content.body =[notification.userInfo objectForKey:@"messageText"];
+    content.sound = [UNNotificationSound defaultSound];
+    content.userInfo= notification.userInfo;
+    
+    UNNotificationRequest* request = [UNNotificationRequest requestWithIdentifier:[NSUUID alloc].UUIDString
+                                                                          content:content trigger:nil];
+    
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    
+    [center addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
+        
+    }];
+}
+
+-(void) showLegacyNotification:(NSNotification *)notification
+{
+    
+    NSString* acctString =[NSString stringWithFormat:@"%ld", (long)[[notification.userInfo objectForKey:@"accountNo"] integerValue]];
+    NSString* fullName =[[DataLayer sharedInstance] fullName:[notification.userInfo objectForKey:@"from"] forAccount:acctString];
+    
+    NSString* nameToShow=[notification.userInfo objectForKey:@"from"];
+    if([fullName length]>0) nameToShow=fullName;
+    NSDate* theDate=[NSDate dateWithTimeIntervalSinceNow:0]; //immediate fire
+    
+    UIApplication* app = [UIApplication sharedApplication];
+    NSArray*    oldNotifications = [app scheduledLocalNotifications];
+    
+    // Clear out the old notification before scheduling a new one.
+    if ([oldNotifications count] > 0)
+        [app cancelAllLocalNotifications];
+    
+    // Create a new notification
+    UILocalNotification* alarm = [[UILocalNotification alloc] init];
+    if (alarm)
+    {
+        //scehdule info
+        alarm.fireDate = theDate;
+        alarm.timeZone = [NSTimeZone defaultTimeZone];
+        alarm.repeatInterval = 0;
+        alarm.category=@"Reply";
+        
+        if([[NSUserDefaults standardUserDefaults] boolForKey:@"MessagePreview"]) {
+            alarm.alertTitle  =nameToShow;
+            alarm.alertBody =[notification.userInfo objectForKey:@"messageText"];
+        }  else {
+            alarm.alertTitle =  nameToShow;
+        }
+        
+        if( [[NSUserDefaults standardUserDefaults] boolForKey:@"Sound"]==true)
+        {
+            NSString *filename = [[NSUserDefaults standardUserDefaults] objectForKey:@"AlertSoundFile"];
+            if(filename) {
+                alarm.soundName=[NSString stringWithFormat:@"AlertSounds/%@.aif",filename];
+            } else  {
+                alarm.soundName=UILocalNotificationDefaultSoundName;
+            }
+        }
+        alarm.userInfo=notification.userInfo;
+        [app scheduleLocalNotification:alarm];
+        DDLogVerbose(@"Scheduled local message alert ");
+    }
+}
+
+-(void) presentAlert:(NSNotification *)notification
+{
+    if(([UIApplication sharedApplication].applicationState==UIApplicationStateBackground)
+       || ([UIApplication sharedApplication].applicationState==UIApplicationStateInactive ))
+    {
+        if(SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"10.0")){
+            [self showModernNotificaion:notification];
+        }
+        else  {
+            [self showLegacyNotification:notification];
+        }
+    }
+    else
+    {
+        if(!([[notification.userInfo objectForKey:@"from"] isEqualToString:self.currentContact]) &&
+           !([[notification.userInfo objectForKey:@"to"] isEqualToString:self.currentContact] ) )
+            //  &&![[notification.userInfo objectForKey:@"from"] isEqualToString:@"Info"]
+        {
+            if(SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"10.0")){
+                [self showModernNotificaion:notification];
+            } else  {
+                NSString* acctString =[NSString stringWithFormat:@"%ld", (long)[[notification.userInfo objectForKey:@"accountNo"] integerValue]];
+                NSString* fullName =[[DataLayer sharedInstance] fullName:[notification.userInfo objectForKey:@"from"] forAccount:acctString];
+                
+                NSString* nameToShow=[notification.userInfo objectForKey:@"from"];
+                if([fullName length]>0) nameToShow=fullName;
+                NSDate* theDate=[NSDate dateWithTimeIntervalSinceNow:0]; //immediate fire
+                
+                SlidingMessageViewController* slidingView= [[SlidingMessageViewController alloc] correctSliderWithTitle:nameToShow message:[notification.userInfo objectForKey:@"messageText"] user:[notification.userInfo objectForKey:@"from"] account:[notification.userInfo objectForKey:@"accountNo"] ];
+                
+                [self.window addSubview:slidingView.view];
+                
+                [slidingView showMsg];
+            }
+        }
+    }
+    
+};
 
 -(void) dealloc
 {

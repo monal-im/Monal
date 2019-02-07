c13ce518f1351121cafacaf7b9e9494c415d7f18
Anurodh Pokharel <info@monal.im>
2019-02-07 16:24:34 -0500

    added clean tokens option

--- Monal/Classes/MLNotificationSettingsViewController.m
+++ Monal/Classes/MLNotificationSettingsViewController.m
@@ -17,8 +17,11 @@ NS_ENUM(NSInteger, kNotificationSettingSection)
     kNotificationSettingSectionUser,
     kNotificationSettingSectionMonalPush,
     kNotificationSettingSectionAccounts,
+    kNotificationSettingSectionAdvanced,
     kNotificationSettingSectionCount
 };
+
+static const int ddLogLevel = LOG_LEVEL_VERBOSE;
 
 @interface MLNotificationSettingsViewController ()
 @property (nonatomic, strong) NSArray *sectionsHeaders;
@@ -40,20 +43,20 @@ NS_ENUM(NSInteger, kNotificationSettingSection)
     self.sectionsFooters =@[@"Apple push service should always be on. If it is off, your device can not talk to Apple's server.",
                      @"If Monal can't show notifications, you will not see alerts when a message arrives. This happens if you tapped 'Decline' when Monal first asked permission.  Fix it by going to iOS Settings -> Monal -> Notifications and select 'Allow Notifications'. ",
                      @"If Monal push is off, your device could not talk to push.monal.im. This should also never be off. It requires Apple push service to work first. ",
-                     @""];
+                     @"",
+                            @"Rebuilding is useful if you are not expereicing problems. This will require an app restart to work."];
     
     self.sectionsHeaders =@[@"",
                             @"",
                             @"",
-                            @"Accounts"];
+                            @"Accounts",
+                            @"Advanced"];
     
     self.apple=@[@"Apple Push Service"];
-     self.user=@[@"Can Show Notifications"];
-     self.monal=@[@"Monal Push Server"];
-   
+    self.user=@[@"Can Show Notifications"];
+    self.monal=@[@"Monal Push Server"];
     
     self.splitViewController.preferredDisplayMode=UISplitViewControllerDisplayModeAllVisible;
-    
 }
 
 -(void) viewWillAppear:(BOOL)animated
@@ -104,6 +107,10 @@ NS_ENUM(NSInteger, kNotificationSettingSection)
             
         case kNotificationSettingSectionAccounts: {
             toreturn= [MLXMPPManager sharedInstance].connectedXMPP.count;
+            break;
+        }
+        case kNotificationSettingSectionAdvanced: {
+            toreturn=1;
             break;
         }
             
@@ -186,6 +193,14 @@ NS_ENUM(NSInteger, kNotificationSettingSection)
             break;
         }
             
+        case kNotificationSettingSectionAdvanced: {
+            MLSwitchCell *cell= [tableView dequeueReusableCellWithIdentifier:@"switchCell"];
+            cell.toggleSwitch.hidden=YES;
+            cell.cellLabel.text =@"Rebuild Tokens";
+            toreturn=cell;
+            break;
+        }
+            
     }
     
     return toreturn;
@@ -193,7 +208,63 @@ NS_ENUM(NSInteger, kNotificationSettingSection)
 }
 
 
-
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:NO];
+    switch(indexPath.section)
+    {
+        case kNotificationSettingSectionAdvanced: {
+            
+            NSString *node = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
+            
+            NSString *post = [NSString stringWithFormat:@"type=apns&node=%@", [node stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]]];
+            NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+            NSString *postLength = [NSString stringWithFormat:@"%luld",[postData length]];
+            
+            NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+            //this is the hardcoded push api endpoint
+            [request setURL:[NSURL URLWithString:@"https://push.monal.im:5281/push_appserver/v1/unregister"]];
+            [request setHTTPMethod:@"POST"];
+            [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+            [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+            [request setHTTPBody:postData];
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+                    
+                    NSHTTPURLResponse *httpresponse= (NSHTTPURLResponse *) response;
+                    
+                    if(!error && httpresponse.statusCode<400)
+                    {
+                        DDLogInfo(@"connection to push api successful");
+                        
+                        NSString *responseBody = [[NSString alloc] initWithData:data  encoding:NSUTF8StringEncoding];
+                        DDLogInfo(@"push api returned: %@", responseBody);
+                        NSArray *responseParts=[responseBody componentsSeparatedByString:@"\n"];
+                        if(responseParts.count>0){
+                            if([responseParts[0] isEqualToString:@"OK"] )
+                            {
+                                DDLogInfo(@"push api: unregistered");
+                            }
+                            else {
+                                DDLogError(@" push api returned invalid data: %@", [responseParts componentsJoinedByString: @" | "]);
+                            }
+                        } else {
+                            DDLogError(@"push api could  not be broken into parts");
+                        }
+                        
+                    } else
+                    {
+                        DDLogError(@" connection to push api NOT successful");
+                    }
+                    
+                }] resume];
+            });
+            
+            break;
+        }
+    }
+}
 
 
 @end

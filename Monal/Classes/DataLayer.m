9ec6881db52e16ca617651adf6e8e32bf88708ac
Anurodh Pokharel <info@monal.im>
2019-09-05 09:13:56 -0400

    fixed message unread setting

--- Monal/Classes/DataLayer.m
+++ Monal/Classes/DataLayer.m
@@ -1825,8 +1825,8 @@ static DataLayer *sharedInstance=nil;
 -(void) markAsReadBuddy:(NSString*) buddy forAccount:(NSString*) accountNo
 {
     if(!buddy || !accountNo) return;
-    NSString* query2=[NSString stringWithFormat:@"  update message_history set unread=0 where account_id=? and message_from=?"];
-    [self executeNonQuery:query2 andArguments:@[accountNo, buddy] withCompletion:nil];
+    NSString* query2=[NSString stringWithFormat:@"  update message_history set unread=0 where account_id=? and message_from=? or message_to=?"];
+    [self executeNonQuery:query2 andArguments:@[accountNo, buddy, buddy] withCompletion:nil];
 
 }
 

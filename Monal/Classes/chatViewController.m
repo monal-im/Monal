c120b413dd72a016b9df13469dddd21184c14c8f
Anurodh Pokharel <info@monal.im>
2019-09-12 23:01:26 -0400

    fixed typo

--- Monal/Classes/chatViewController.m
+++ Monal/Classes/chatViewController.m
@@ -575,8 +575,8 @@ static const int ddLogLevel = LOG_LEVEL_ERROR;
     if(!self.uploadHUD) {
         self.uploadHUD = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
         self.uploadHUD.removeFromSuperViewOnHide=YES;
-        self.uploadHUD.label.text =@"Uploding";
-        self.uploadHUD.detailsLabel.text =@"Upoading file to server";
+        self.uploadHUD.label.text =@"Uplaoding";
+        self.uploadHUD.detailsLabel.text =@"Uploading file to server";
 
     }
 

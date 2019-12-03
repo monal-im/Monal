bf48187a5505928211fbf260b7875cdb6d91df3e
Anurodh Pokharel <info@monal.im>
2019-12-03 12:01:32 -0500

    update config

--- Monal/Pods/Target Support Files/SignalProtocolObjC-iOS/SignalProtocolObjC-iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SignalProtocolObjC_iOS : NSObject
-@end
-@implementation PodsDummy_SignalProtocolObjC_iOS
-@end
--- Monal/Pods/Target Support Files/TPCircularBuffer-macOS/TPCircularBuffer-macOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TPCircularBuffer_macOS : NSObject
-@end
-@implementation PodsDummy_TPCircularBuffer_macOS
-@end

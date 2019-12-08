47a5bd952b7846d032e3f66d4b8a20bd213d893b
Anurodh Pokharel <info@monal.im>
2019-12-07 21:34:09 -0500

    smaller device

--- Monal/Pods/Headers/Private/Dropbox-OSX-SDK/DropboxOSX/DBJsonWriter.h
+++
@@ -1,1 +0,0 @@
-../../../../Dropbox-OSX-SDK/dropbox-osx-sdk-1.3.13/DropboxSDK/Classes/DropboxSDK/DBJsonWriter.h
--- Monal/Pods/Target Support Files/Dropbox-OSX-SDK/Dropbox-OSX-SDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Dropbox_OSX_SDK : NSObject
-@end
-@implementation PodsDummy_Dropbox_OSX_SDK
-@end

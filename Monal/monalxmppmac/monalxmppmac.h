8f7bc99c8c7aa11da46377a5c90d8792b9403df5
Anurodh Pokharel <info@monal.im>
2019-12-14 23:01:07 -0500

    adding mac framework

---
+++ Monal/monalxmppmac/monalxmppmac.h
@@ -0,0 +1,19 @@
+//
+//  monalxmppmac.h
+//  monalxmppmac
+//
+//  Created by Anurodh Pokharel on 12/14/19.
+//  Copyright © 2019 Monal.im. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for monalxmppmac.
+FOUNDATION_EXPORT double monalxmppmacVersionNumber;
+
+//! Project version string for monalxmppmac.
+FOUNDATION_EXPORT const unsigned char monalxmppmacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <monalxmppmac/PublicHeader.h>
+
+

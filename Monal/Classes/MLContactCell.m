06db968974824df121c57043f829241cbc49f842
Anurodh Pokharel <info@monal.im>
2020-01-20 20:37:15 -0500

    dont update ui if values dont change

--- Monal/Classes/MLContactCell.m
+++ Monal/Classes/MLContactCell.m
@@ -50,42 +50,46 @@
 
 -(void) showStatusText:(NSString *) text
 {
-    self.statusText.text=text;
-    if(text)
-    {
-        self.centeredDisplayName.hidden=YES;
-        self.displayName.hidden=NO;
-        self.statusText.hidden=NO;
-    }
-    else {
-        self.centeredDisplayName.hidden=NO;
-        self.displayName.hidden=YES;
-        self.statusText.hidden=YES;
+    if(![self.statusText.text isEqualToString:text]) {
+        self.statusText.text=text;
+        if(text)
+        {
+            self.centeredDisplayName.hidden=YES;
+            self.displayName.hidden=NO;
+            self.statusText.hidden=NO;
+        }
+        else {
+            self.centeredDisplayName.hidden=NO;
+            self.displayName.hidden=YES;
+            self.statusText.hidden=YES;
+        }
     }
 }
 
 -(void) showDisplayName:(NSString *) name
 {
-    self.centeredDisplayName.text=name;
-    self.displayName.text=name;
+    if(![self.displayName.text isEqualToString:name]){
+        self.centeredDisplayName.text=name;
+        self.displayName.text=name;
+    }
 }
 
 -(void) setCount:(NSInteger)count
 {
-    _count=count;
-    
-    if(_count>0)
-    {
-        self.badge.hidden=NO;
-        [self.badge setTitle:[NSString stringWithFormat:@"%ld", (long)_count] forState:UIControlStateNormal];
-    }
-    else
-    {
-        self.badge.hidden=YES;
-        [self.badge setTitle:@"" forState:UIControlStateNormal];
-    }
-    
-    
+    if(_count!=count) {
+        _count=count;
+        
+        if(_count>0)
+        {
+            self.badge.hidden=NO;
+            [self.badge setTitle:[NSString stringWithFormat:@"%ld", (long)_count] forState:UIControlStateNormal];
+        }
+        else
+        {
+            self.badge.hidden=YES;
+            [self.badge setTitle:@"" forState:UIControlStateNormal];
+        }
+    }    
 }
 
 

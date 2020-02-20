20c72c5739467999a33d9a3fbb36277241838b93
Anurodh Pokharel <info@monal.im>
2020-02-19 20:04:38 -0500

    account not contact

--- Monal/Classes/chatViewController.m
+++ Monal/Classes/chatViewController.m
@@ -766,7 +766,7 @@
         //getting encrypted chat turns it on. nto the other way
         if(message.encrypted) {
             dispatch_async(dispatch_get_main_queue(), ^{
-                [[DataLayer sharedInstance] encryptForJid:self.contact.contactJid andAccountNo:self.contact.contactJid];
+                [[DataLayer sharedInstance] encryptForJid:self.contact.contactJid andAccountNo:self.contact.accountId];
                 self.encryptChat=YES;
                 [self refreshButton:notification];
             });

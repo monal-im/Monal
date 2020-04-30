fd721b3e758ac4ac97696492f79e278191719b1a
Echolon <Echolon@users.noreply.github.com>
2020-05-01 00:34:30 +0200

    Add ActiveCVContr.m, addContact.m

--- Monal/Classes/addContact.m
+++ Monal/Classes/addContact.m
@@ -25,8 +25,8 @@
 {
     if([[MLXMPPManager sharedInstance].connectedXMPP count]==0)
     {
-        UIAlertController *messageAlert =[UIAlertController alertControllerWithTitle:@"No connected accounts" message:@"Please make sure at least one account has connected before trying to add a contact." preferredStyle:UIAlertControllerStyleAlert];
-        UIAlertAction *closeAction =[UIAlertAction actionWithTitle:@"Close" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+        UIAlertController *messageAlert =[UIAlertController alertControllerWithTitle:NSLocalizedString(@"No connected accounts",@"") message:NSLocalizedString(@"Please make sure at least one account has connected before trying to add a contact.",@"") preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *closeAction =[UIAlertAction actionWithTitle:NSLocalizedString(@"Close",@"") style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
             
         }];
         [messageAlert addAction:closeAction];
@@ -46,8 +46,8 @@
             
             [[MLXMPPManager sharedInstance] addContact:contactObj];
            
-            UIAlertController *messageAlert =[UIAlertController alertControllerWithTitle:@"Permission Requested" message:@"The new contact will be added to your contacts list when the person you've added has approved your request." preferredStyle:UIAlertControllerStyleAlert];
-            UIAlertAction *closeAction =[UIAlertAction actionWithTitle:@"Close" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+            UIAlertController *messageAlert =[UIAlertController alertControllerWithTitle:NSLocalizedString(@"Permission Requested",@"") message:NSLocalizedString(@"The new contact will be added to your contacts list when the person you've added has approved your request.",@"") preferredStyle:UIAlertControllerStyleAlert];
+            UIAlertAction *closeAction =[UIAlertAction actionWithTitle:NSLocalizedString(@"Close",@"") style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
                 if(self.completion) self.completion(contactObj);
                 [self dismissViewControllerAnimated:YES completion:nil];
             }];
@@ -58,8 +58,8 @@
         }
         else
         {
-            UIAlertController *messageAlert =[UIAlertController alertControllerWithTitle:@"Error" message:@"Name can't be empty" preferredStyle:UIAlertControllerStyleAlert];
-            UIAlertAction *closeAction =[UIAlertAction actionWithTitle:@"Close" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+            UIAlertController *messageAlert =[UIAlertController alertControllerWithTitle:NSLocalizedString(@"Error",@"") message:NSLocalizedString(@"Name can't be empty",@"") preferredStyle:UIAlertControllerStyleAlert];
+            UIAlertAction *closeAction =[UIAlertAction actionWithTitle:NSLocalizedString(@"Close",@"") style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
                 
             }];
             [messageAlert addAction:closeAction];
@@ -93,8 +93,8 @@
 -(void) viewDidLoad
 {
     [super viewDidLoad];
-    self.navigationItem.title=@"Add Contact";
-    _closeButton =[[UIBarButtonItem alloc] initWithTitle:@"Close" style:UIBarButtonItemStylePlain target:self action:@selector(closeView)];
+    self.navigationItem.title=NSLocalizedString(@"Add Contact",@"");
+    _closeButton =[[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Close",@"") style:UIBarButtonItemStylePlain target:self action:@selector(closeView)];
     self.navigationItem.rightBarButtonItem=_closeButton;
         
     [self.tableView registerNib:[UINib nibWithNibName:@"MLTextInputCell"
@@ -126,7 +126,7 @@
 {
     if(section==0)
     {
-        return @"Contacts are usually in the format: username@domain.something";
+        return NSLocalizedString(@"Contacts are usually in the format: username@domain.something",@"");
     }
     else return nil;
 }
@@ -157,14 +157,14 @@
         case 0: {
             if(indexPath.row ==0){
                 UITableViewCell *accountCell =[tableView dequeueReusableCellWithIdentifier:@"AccountCell"];
-                accountCell.textLabel.text=[NSString stringWithFormat:@"Using Account: %@", [[MLXMPPManager sharedInstance] getAccountNameForConnectedRow:_selectedRow]];
+                accountCell.textLabel.text=[NSString stringWithFormat:NSLocalizedString(@"Using Account: %@",@""), [[MLXMPPManager sharedInstance] getAccountNameForConnectedRow:_selectedRow]];
                 accountCell.accessoryType=UITableViewCellAccessoryDisclosureIndicator;
                 cell=accountCell;
             }
             else   if(indexPath.row ==1){
                 MLTextInputCell *textCell =[tableView dequeueReusableCellWithIdentifier:@"TextCell"];
                 self.contactName =textCell.textInput;
-                self.contactName.placeholder = @"Contact Name";
+                self.contactName.placeholder = NSLocalizedString(@"Contact Name",@"");
                 self.contactName.delegate=self;
                 cell= textCell;
             }

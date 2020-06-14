c6165c7b77f1ab3c83a206e96027cd526d298ff4
Thilo Molitor <thilo@eightysoft.de>
2020-06-14 19:45:33 +0200

    Make change detection race-free

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -4,4 +4,4 @@
 #cat build/ipa/DistributionSummary.plist
 #cat build/ipa/ExportOptions.plist
 ls -l build/ipa/$APP_NAME.ipa
-curl -X POST -F "datei=@build/ipa/$APP_NAME.ipa" -H "X-Secret: $KEY_PASSWORD" https://www.eightysoft.de/monal/upload.php
+curl -X POST -F "datei=@build/ipa/$APP_NAME.ipa" -F "changes=@../changes.txt" -H "X-Secret: $KEY_PASSWORD" https://www.eightysoft.de/monal/upload.php

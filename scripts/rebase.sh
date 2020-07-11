2bd33b696619af12bd5cd001f786e94812906044
Thilo Molitor <thilo@eightysoft.de>
2020-07-11 11:40:17 +0200

    Add rebasing script to repo instead of github webhook script

---
+++ scripts/rebase.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+set -x
+PS4='${LINENO}: '
+date
+git config push.default simple
+#git config user.email thilo@eightysoft.de
+git config user.name 'Rebase-Bot'
+git remote add upstream https://github.com/tmolitor-stud-tu/Monal
+git fetch upstream >/dev/null
+date
+git branch tmpcopy
+git log develop..upstream/develop > ../changes.txt
+first="$(git log --grep='Create LICENSE' --since='Mon Jun 8 16:33:25 2020 +0200' --author='tmolitor-stud-tu' --oneline --no-abbrev-commit tmpcopy | awk '{print $1}')"
+git reset --hard upstream/develop
+ls -l ../changes.txt
+mv ../changes.txt changes.txt
+git add changes.txt
+git commit -m "Add newest changes.txt"
+git cherry-pick $first^..tmpcopy
+#this would contain old commits now rebased/changed in Monal
+#git cherry-pick develop..tmpcopy
+git branch -D tmpcopy
+date
+git push --force

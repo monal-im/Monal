8a0c04662f1041f0d13e8d140b799e292a3a3f45
Thilo Molitor <thilo@eightysoft.de>
2020-07-20 11:25:15 +0200

    Upload iOS and Catalyst files in one shot

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 ls -l build/ipa/$APP_NAME.ipa
-curl -X POST -F "datei=@build/ipa/$APP_NAME.ipa" -F "changes=@../changes.txt" -H "X-Upload: ios" -H "X-Secret: $KEY_PASSWORD" https://www.eightysoft.de/monal/upload.php
 ls -l build/app/$APP_NAME.tar
-curl -X POST -F "datei=@build/app/$APP_NAME.tar" -F "changes=@../changes.txt" -H "X-Upload: macos" -H "X-Secret: $KEY_PASSWORD" https://www.eightysoft.de/monal/upload.php
+curl -X POST -F "ios=@build/ipa/$APP_NAME.ipa" -F "mac=@build/app/$APP_NAME.tar" -F "changes=@../changes.txt" -H "X-Secret: $KEY_PASSWORD" https://www.eightysoft.de/monal/upload.php

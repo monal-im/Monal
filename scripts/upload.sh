036367a585f8a811f168590a77a6214140f2879a
Thilo Molitor <thilo@eightysoft.de>
2020-07-21 18:42:06 +0200

    Temporarily disable macOS build

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 ls -l build/ipa/$APP_NAME.ipa
-ls -l build/app/$APP_NAME.tar
-curl -X POST -F "ios=@build/ipa/$APP_NAME.ipa" -F "mac=@build/app/$APP_NAME.tar" -F "changes=@../changes.txt" -H "X-Secret: $KEY_PASSWORD" https://www.eightysoft.de/monal/upload.php
+#ls -l build/app/$APP_NAME.tar
+#curl -X POST -F "ios=@build/ipa/$APP_NAME.ipa" -F "mac=@build/app/$APP_NAME.tar" -F "changes=@../changes.txt" -H "X-Secret: $KEY_PASSWORD" https://www.eightysoft.de/monal/upload.php
+
+curl -X POST -F "ios=@build/ipa/$APP_NAME.ipa" -F "changes=@../changes.txt" -H "X-Secret: $KEY_PASSWORD" https://www.eightysoft.de/monal/upload.php

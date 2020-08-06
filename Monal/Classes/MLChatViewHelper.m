d00aa85e04b699902e49bb2046bae8113efab4ef
Friedrich Altheide <11352905+FriedrichAltheide@users.noreply.github.com>
2020-08-06 21:37:09 +0200

    Only show omemo alert when no devices keys are detected and we want to activate omemo

--- Monal/Classes/MLChatViewHelper.m
+++ Monal/Classes/MLChatViewHelper.m
@@ -12,16 +12,8 @@
 @implementation MLChatViewHelper
 
 +(void) toggleEncryption:(BOOL*) currentState forAccount:(NSString*) accountNo forContactJid:(NSString*) contactJid withKnownDevices:(NSArray*) knownDevies withSelf:(id) andSelf afterToggle:(void (^)(void)) afterToggle {
-    if(knownDevies.count > 0) {
-        if(*currentState) {
-            [[DataLayer sharedInstance] disableEncryptForJid:contactJid andAccountNo:accountNo];
-        } else {
-            [[DataLayer sharedInstance] encryptForJid:contactJid andAccountNo:accountNo];
-        }
-        *currentState = !(*currentState);
-        afterToggle();
-    } else  {
-        // Show a warning when no device keys could be found -> encryption is not possible
+    if(knownDevies.count == 0 && *currentState == NO) {
+        // Show a warning when no device keys could be found and the user tries to enable encryption -> encryption is not possible
         UIAlertController* alert = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"Encryption Not Supported", @"") message:NSLocalizedString(@"This contact does not appear to have any devices that support encryption.", @"") preferredStyle:UIAlertControllerStyleAlert];
         [alert addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"Close", @"") style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
             [alert dismissViewControllerAnimated:YES completion:nil];
@@ -29,6 +21,16 @@
 
         // open the alert msg in the calling view controller
         [andSelf presentViewController:alert animated:YES completion:nil];
+    } else {
+        if(*currentState) {
+            [[DataLayer sharedInstance] disableEncryptForJid:contactJid andAccountNo:accountNo];
+        } else {
+            [[DataLayer sharedInstance] encryptForJid:contactJid andAccountNo:accountNo];
+        }
+        // Update the encryption value in the caller class
+        *currentState = !(*currentState);
+        // Call the code that should update the UI elements
+        afterToggle();
     }
 }
 

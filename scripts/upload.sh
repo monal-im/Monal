e3b6c2033d20ca524bb91b59f4098a982f448d51
Friedrich Altheide <11352905+FriedrichAltheide@users.noreply.github.com>
2020-08-08 08:18:54 +0200

    Allow macos build via env

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 ls -l build/ipa/$APP_NAME.ipa
-#ls -l build/app/$APP_NAME.tar
-#curl -X POST -F "ios=@build/ipa/$APP_NAME.ipa" -F "mac=@build/app/$APP_NAME.tar" -F "changes=@../changes.txt" -H "X-Secret: $KEY_PASSWORD" https://www.eightysoft.de/monal/upload.php
-
-curl -X POST -F "ios=@build/ipa/$APP_NAME.ipa" -F "changes=@../changes.txt" -H "X-Secret: $KEY_PASSWORD" https://www.eightysoft.de/monal/upload.php
+if [ "$BUILD_MACOS" = true ] || grep -q "Friedrich" "$TRAVIS_BUILD_DIR/changes.txt"
+then
+	ls -l build/app/$APP_NAME.tar
+	curl -X POST -F "ios=@build/ipa/$APP_NAME.ipa" -F "mac=@build/app/$APP_NAME.tar" -F "changes=@../changes.txt" -H "X-Secret: $KEY_PASSWORD" https://www.eightysoft.de/monal/upload.php
+else
+	curl -X POST -F "ios=@build/ipa/$APP_NAME.ipa" -F "changes=@../changes.txt" -H "X-Secret: $KEY_PASSWORD" https://www.eightysoft.de/monal/upload.php
+fi

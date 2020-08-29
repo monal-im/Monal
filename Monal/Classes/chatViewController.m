c5e182094a5fc911f8d705a025034f5678f9bade
Friedrich Altheide <11352905+FriedrichAltheide@users.noreply.github.com>
2020-08-29 20:47:37 +0200

    Speedup mark as read

--- Monal/Classes/chatViewController.m
+++ Monal/Classes/chatViewController.m
@@ -406,10 +406,7 @@ enum chatViewControllerSections {
     }
 #endif
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [self refreshCounter];
-    });
-    
+    [self refreshCounter];
 }
 
 -(void) viewWillDisappear:(BOOL)animated

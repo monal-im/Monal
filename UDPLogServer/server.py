cc62cf9a73530edb5540b490afca78c9a426aebe
Thilo Molitor <thilo@eightysoft.de>
2020-09-18 21:11:14 +0200

    Add support for IPv6
    This supports simultaneous IPv6 and IPv4 listening in UDPLogServer,
    too.

--- UDPLogServer/server.py
+++ UDPLogServer/server.py
@@ -2,6 +2,7 @@
 import sys
 import argparse
 import socket
+import ipaddress
 import json
 import zlib
 import hashlib
@@ -13,11 +14,11 @@ def eprint(*args, **kwargs):
 def decrypt(ciphertext, key):
     iv = ciphertext[:12]
     if len(iv) != 12:
-        raise DataIntegrityError("Cipher text is damaged: invalid iv length")
+        raise Exception("Cipher text is damaged: invalid iv length")
 
     tag = ciphertext[12:28]
     if len(tag) != 16:
-        raise DataIntegrityError("Cipher text is damaged: invalid tag length")
+        raise Exception("Cipher text is damaged: invalid tag length")
 
     encrypted = ciphertext[28:]
 
@@ -28,13 +29,13 @@ def decrypt(ciphertext, key):
     try:
         plaintext = cipher.decrypt_and_verify(encrypted, tag)
     except ValueError as e:
-        raise DataIntegrityError("Cipher text is damaged: {}".format(e))
+        raise Exception("Cipher text is damaged: {}".format(e))
     return plaintext
 
 # parse commandline
 parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, description="Monal UDP-Logserver.", epilog="WARNING: WE DO NOT ENHANCE ENTROPY!! PLEASE MAKE SURE TO USE A ENCRYPTION KEY WITH PROPER ENTROPY!!")
 parser.add_argument("-k", "--key", type=str, required=True, metavar='KEY', help="AES-Key to use for decription of incoming data")
-parser.add_argument("-l", "--listen", type=str, metavar='HOSTNAME', help="Local hostname or IP to listen on (Default: 0.0.0.0 e.g. any)", default="0.0.0.0")
+parser.add_argument("-l", "--listen", type=str, metavar='HOSTNAME', help="Local hostname or IP to listen on (Default: :: e.g. any)", default="::")
 parser.add_argument("-p", "--port", type=int, metavar='PORT', help="Port to listen on (Default: 5555)", default=5555)
 args = parser.parse_args()
 
@@ -44,7 +45,7 @@ m.update(bytes(args.key, "UTF-8"))
 key = m.digest()
 
 # create listening udp socket and process all incoming packets
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET6 if ipaddress.ip_address(args.listen).version==6 else socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((args.listen, args.port))
 while True:
     # receive raw udp packet

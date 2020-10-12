09a704e031b23ebb3121860474e9a2b82eef023c
Thilo Molitor <thilo@eightysoft.de>
2020-10-12 13:42:24 +0200

    Fix UDP logger bug

--- Monal/Classes/MLUDPLogger.m
+++ Monal/Classes/MLUDPLogger.m
@@ -150,10 +150,11 @@
     addr.sin_port           = htons([[[HelperTools defaultsDB] stringForKey:@"udpLoggerPort"] integerValue]);
     addr.sin_addr.s_addr    = inet_addr([[[HelperTools defaultsDB] stringForKey:@"udpLoggerHostname"] UTF8String]);
     
-    //send log via udp
-    CFSocketError error = CFSocketSendData(_cfsocketout, (__bridge CFDataRef)[NSData dataWithBytes:(const UInt8*)&addr length:sizeof(addr)], (__bridge CFDataRef)data, 0);
-    if(error)
+    if(!CFSocketIsValid(_cfsocketout))
     {
+        CFSocketInvalidate(_cfsocketout);       //just to make sure
+        //release old socket object and create new one
+        CFRelease(_cfsocketout);
         _cfsocketout = CFSocketCreate(
             kCFAllocatorDefault,
             PF_INET,
@@ -163,12 +164,11 @@
             NULL,
             NULL
         );
-        
-        //try again
-        CFSocketError error = CFSocketSendData(_cfsocketout, (__bridge CFDataRef)[NSData dataWithBytes:(const UInt8*)&addr length:sizeof(addr)], (__bridge CFDataRef)data, 0);
-        if(error)
-            NSLog(@"MLUDPLogger CFSocketSendData error: %ld", (long)error);
     }
+    //send log via udp
+    CFSocketError error = CFSocketSendData(_cfsocketout, (__bridge CFDataRef)[NSData dataWithBytes:(const UInt8*)&addr length:sizeof(addr)], (__bridge CFDataRef)data, 0);
+    if(error)
+        NSLog(@"MLUDPLogger CFSocketSendData error: %ld", (long)error);
 }
 
 @end

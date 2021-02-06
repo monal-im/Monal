0e96f96f1e963e19e6fce0cae52ea6d88c522771
Friedrich Altheide <11352905+FriedrichAltheide@users.noreply.github.com>
2021-02-06 17:36:52 +0100

    rebase.sh: speedup fetch

--- scripts/rebase.sh
+++ scripts/rebase.sh
@@ -6,12 +6,15 @@ date
 git config push.default simple
 #git config user.email thilo@eightysoft.de
 git config user.name 'Rebase-Bot'
-git fetch origin
+git fetch origin alpha.build
+git fetch origin develop
+git pull --unshallow
 date
 git branch tmpcopy
 git log origin/alpha.build..origin/develop > ../changes.txt
-first="$(git log --grep='\*\*\* INITIAL ALPHA COMMIT \*\*\*' --since='Mon Jun 8 16:33:25 2020 +0200' --author='tmolitor-stud-tu' --oneline --no-abbrev-commit tmpcopy | awk '{print $1}')"
 git reset --hard origin/develop
+first="$(git log --grep='\*\*\* INITIAL ALPHA COMMIT \*\*\*' --since='Mon Jun 8 16:33:25 2020 +0200' --author='tmolitor-stud-tu' --oneline --no-abbrev-commit tmpcopy | awk '{print $1}')"
+echo $first
 touch ../changes.txt
 ls -l ../changes.txt
 mv ../changes.txt changes.txt
@@ -22,4 +25,5 @@ git cherry-pick $first^..tmpcopy
 #git cherry-pick develop..tmpcopy
 git branch -D tmpcopy
 date
+git remote set-url origin https://github.com/monal-im/Monal.git
 git push --force-with-lease

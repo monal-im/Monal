f2f26dbaf722ca02ac6a5617a8e0f61b0cbca20d
Thilo Molitor <thilo@eightysoft.de>
2021-02-06 18:20:21 +0100

    update upload script

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -4,4 +4,4 @@ cd Monal
 
 ls -l build/ipa/$APP_NAME.ipa
 ls -l build/app/$APP_NAME.tar
-curl -X POST -F "ios=@build/ipa/$APP_NAME.ipa" -F "mac=@build/app/$APP_NAME.tar" -F "changes=@../changes.txt" -H "X-Secret: $KEY_PASSWORD" https://www.eightysoft.de/monal/upload.php
+curl -X POST -F "ios=@build/ipa/$APP_NAME.ipa" -F "mac=@build/app/$APP_NAME.tar" -F "changes=@../changes.txt" -H "X-Secret: $ALPHA_UPLOAD_SECRET" https://www.eightysoft.de/monal/upload.php

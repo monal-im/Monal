4929b9a6307303490248d63a7f73154bce2b84c0
Thilo Molitor <thilo@eightysoft.de>
2021-02-06 17:11:39 +0100

    change rebase script to monal-im org

--- scripts/rebase.sh
+++ scripts/rebase.sh
@@ -6,13 +6,15 @@ date
 git config push.default simple
 #git config user.email thilo@eightysoft.de
 git config user.name 'Rebase-Bot'
-git remote add upstream https://github.com/tmolitor-stud-tu/Monal
-git fetch upstream >/dev/null
+git fetch origin/develop
+git fetch origin/alpha.build
 date
 git branch tmpcopy
-git log develop..upstream/develop > ../changes.txt
+git log origin/alpha.build..origin/develop > ../changes.txt
 first="$(git log --grep='\*\*\* INITIAL ALPHA COMMIT \*\*\*' --since='Mon Jun 8 16:33:25 2020 +0200' --author='tmolitor-stud-tu' --oneline --no-abbrev-commit tmpcopy | awk '{print $1}')"
-git reset --hard upstream/develop
+echo "FIRST: $first"
+exit 0
+git reset --hard origin/develop
 touch ../changes.txt
 ls -l ../changes.txt
 mv ../changes.txt changes.txt
@@ -22,6 +24,5 @@ git cherry-pick $first^..tmpcopy
 #this would contain old commits now rebased/changed in Monal
 #git cherry-pick develop..tmpcopy
 git branch -D tmpcopy
-git remote remove upstream
 date
 git push --force-with-lease

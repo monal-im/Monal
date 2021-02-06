cb7f6c6415be21a29e7ed5b1566e19629a727110
Friedrich Altheide <11352905+FriedrichAltheide@users.noreply.github.com>
2021-02-06 16:45:49 +0100

    Changes for github actions

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
+cd Monal
+
 ls -l build/ipa/$APP_NAME.ipa
-if [ "$BUILD_MACOS" = true ] || grep -q "Friedrich" "$TRAVIS_BUILD_DIR/changes.txt"
-then
-	ls -l build/app/$APP_NAME.tar
-	curl -X POST -F "ios=@build/ipa/$APP_NAME.ipa" -F "mac=@build/app/$APP_NAME.tar" -F "changes=@../changes.txt" -H "X-Secret: $KEY_PASSWORD" https://www.eightysoft.de/monal/upload.php
-else
-	curl -X POST -F "ios=@build/ipa/$APP_NAME.ipa" -F "changes=@../changes.txt" -H "X-Secret: $KEY_PASSWORD" https://www.eightysoft.de/monal/upload.php
-fi
+ls -l build/app/$APP_NAME.tar
+curl -X POST -F "ios=@build/ipa/$APP_NAME.ipa" -F "mac=@build/app/$APP_NAME.tar" -F "changes=@../changes.txt" -H "X-Secret: $KEY_PASSWORD" https://www.eightysoft.de/monal/upload.php

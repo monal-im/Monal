3b48f371d18f70664cb8582284ef8196a1744fd6
Friedrich Altheide <11352905+FriedrichAltheide@users.noreply.github.com>
2021-02-12 17:15:01 +0100

    Monal/Classes/MLContactCell.m: style fixes

--- Monal/Classes/MLContactCell.m
+++ Monal/Classes/MLContactCell.m
@@ -23,8 +23,9 @@
 
 -(void) showStatusText:(NSString *) text
 {
-    if(![self.statusText.text isEqualToString:text]) {
-        self.statusText.text=text;
+    if(![self.statusText.text isEqualToString:text])
+    {
+        self.statusText.text = text;
         [self setStatusTextLayout:text];
     }
 }
@@ -32,22 +33,26 @@
 -(void) showStatusTextItalic:(NSString *) text withItalicRange:(NSRange)italicRange
 {
     UIFont* italicFont = [UIFont italicSystemFontOfSize:self.statusText.font.pointSize];
-    NSMutableAttributedString *italicString = [[NSMutableAttributedString alloc] initWithString:text];
+    NSMutableAttributedString* italicString = [[NSMutableAttributedString alloc] initWithString:text];
     [italicString addAttribute:NSFontAttributeName value:italicFont range:italicRange];
 
-    if(![italicString isEqualToAttributedString:self.statusText.originalAttributedText]) {
+    if(![italicString isEqualToAttributedString:self.statusText.originalAttributedText])
+    {
         self.statusText.attributedText = italicString;
         [self setStatusTextLayout:text];
     }
 }
 
--(void) setStatusTextLayout:(NSString *) text {
-    if(text) {
-        self.centeredDisplayName.hidden=YES;
-        self.displayName.hidden=NO;
-        self.statusText.hidden=NO;
-    } else {
-        self.centeredDisplayName.hidden=NO;
+-(void) setStatusTextLayout:(NSString *) text
+{
+    if(text)
+    {
+        self.centeredDisplayName.hidden = YES;
+        self.displayName.hidden = NO;
+        self.statusText.hidden = NO;
+    } else
+    {
+        self.centeredDisplayName.hidden = NO;
         self.displayName.hidden=YES;
         self.statusText.hidden=YES;
     }
@@ -56,24 +61,27 @@
 -(void) showDisplayName:(NSString *) name
 {
     if(![self.displayName.text isEqualToString:name]){
-        self.centeredDisplayName.text=name;
-        self.displayName.text=name;
+        self.centeredDisplayName.text = name;
+        self.displayName.text = name;
     }
 }
 
 -(void) setCount:(NSInteger)count
 {
-    if(_count!=count) {
-        _count=count;
-        if(_count>0)
+    if(_count != count)
+    {
+        _count = count;
+        if(_count > 0)
         {
-            self.badge.hidden=NO;
+            self.badge.hidden = NO;
             [self.badge setTitle:[NSString stringWithFormat:@"%ld", (long)_count] forState:UIControlStateNormal];
         }
     }
     else{
-        if(!self.badge.hidden) { //handle initial load
-            self.badge.hidden=YES;
+        if(!self.badge.hidden)
+        {
+            //handle initial load
+            self.badge.hidden = YES;
             [self.badge setTitle:@"" forState:UIControlStateNormal];
         }
     }
@@ -84,7 +92,7 @@
     self.isPinned = pinned;
     
     if(pinned) {
-        self.backgroundColor =  [UIColor colorNamed:@"activeChatsPinnedColor"];
+        self.backgroundColor = [UIColor colorNamed:@"activeChatsPinnedColor"];
     } else {
         self.backgroundColor = UIColor.clearColor;
     }

f3378fb2933eec36454dc65f7a6ecb8bd8a90e72
Friedrich Altheide <11352905+FriedrichAltheide@users.noreply.github.com>
2021-03-30 09:03:01 +0200

    Moved MLCrypto into monalxmpp

---
+++ Monal/Classes/MLCrypto.swift
@@ -0,0 +1,77 @@
+//
+//  MLCrypto.swift
+//  monalxmpp
+//
+//  Created by Anurodh Pokharel on 1/7/20.
+//  Copyright © 2020 Monal.im. All rights reserved.
+//
+
+import UIKit
+import CryptoKit
+
+@objcMembers
+public class MLCrypto: NSObject {
+   
+    public func encryptGCM (key: Data, decryptedContent:Data) -> EncryptedPayload?
+    {
+        if #available(iOS 13.0, *) {
+            let gcmKey = SymmetricKey.init(data: key)
+            
+            let iv = AES.GCM.Nonce()
+            
+            do {
+                let encrypted = try AES.GCM.seal(decryptedContent, using: gcmKey, nonce: iv)
+                let encryptedPayload = EncryptedPayload()
+                let combined = encrypted.combined
+                let ciphertext = encrypted.ciphertext
+                
+                if let combined = combined {
+                let ivData = combined.subdata(in: 0..<12)
+                //combined is in the format
+                //iv+body+auth
+                let range = 12+ciphertext.count..<combined.count //16 is gnereally tag size apple uses
+                let tagData = combined.subdata(in:range)
+                
+                encryptedPayload.updateValues(body:ciphertext, iv: ivData, key:key, tag:tagData)
+                encryptedPayload.combined = combined
+                return encryptedPayload
+                } else  {
+                    return nil;
+                }
+            } catch  {
+                return nil
+            }
+        } else {
+            assert(false);
+            return nil;
+        }
+    }
+    
+    // generate 12 byte nonce
+    public func genIV() -> Data?
+    {
+        if #available(iOS 13.0, *) {
+            return Data(AES.GCM.Nonce())
+        } else {
+            assert(false);
+            return nil;
+        }
+    }
+    
+    public func decryptGCM (key: Data, encryptedContent:Data) -> Data?
+    {
+        if #available(iOS 13.0, *) {
+            do {
+                let sealedBoxToOpen = try! AES.GCM.SealedBox(combined: encryptedContent)
+                let gcmKey = SymmetricKey.init(data: key)
+                let decryptedData = try AES.GCM.open(sealedBoxToOpen, using: gcmKey)
+                return decryptedData
+            } catch {
+                return nil;
+            }
+        } else {
+            assert(false);
+            return nil
+        }
+    }
+}
--- Monal/monalxmpp/monalxmpp.h
+++ Monal/monalxmpp/monalxmpp.h
@@ -13,8 +13,3 @@ FOUNDATION_EXPORT double monalxmppVersionNumber;
 
 //! Project version string for monalxmpp.
 FOUNDATION_EXPORT const unsigned char monalxmppVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <monalxmpp/PublicHeader.h>
-
-#import <monalxmppmac/MLConstants.h>
-#import <monalxmpp/DataLayer.h>

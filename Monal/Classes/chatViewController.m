caa6394f1f58131c18c071b0220225a6d491a0b4
Friedrich Altheide <11352905+FriedrichAltheide@users.noreply.github.com>
2021-04-09 11:57:28 +0200

    chatView: display correct group chat member count

--- Monal/Classes/chatViewController.m
+++ Monal/Classes/chatViewController.m
@@ -469,8 +469,9 @@ enum msgSentState {
     jidLabelText = contactDisplayName;
 
     if(self.contact.isGroup) {
-        NSArray* members = [[DataLayer sharedInstance] resourcesForContact:self.contact.contactJid];
-        jidLabelText = [NSString stringWithFormat:@"%@ (%ld)", contactDisplayName, members.count];
+        NSArray* members = [[DataLayer sharedInstance] getMembersAndParticipantsOfMuc:self.contact.contactJid forAccountId:self.xmppAccount.accountNo];
+        if(members.count > 0)
+            jidLabelText = [NSString stringWithFormat:@"%@ (%ld)", contactDisplayName, members.count];
     }
     // change text values
     dispatch_async(dispatch_get_main_queue(), ^{

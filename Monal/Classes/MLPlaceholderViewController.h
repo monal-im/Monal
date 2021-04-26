c5fab3d3a62a74c94f089bd4c168a66908a5fb7d
JimTsai <poormusic2001@gmail.com>
2021-04-26 16:32:55 +0800

    Fix issue #614 (#624)
    * Fix issue #614
    
    Catalyst: custom background not shown after launch of monal #614

--- Monal/Classes/MLPlaceholderViewController.h
+++ Monal/Classes/MLPlaceholderViewController.h
@@ -7,6 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "HelperTools.h"
+#import "MLImageManager.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
--- Monal/Classes/MLPlaceholderViewController.m
+++ Monal/Classes/MLPlaceholderViewController.m
@@ -10,6 +10,8 @@
 
 @interface MLPlaceholderViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *backgroundImageView;
+
 @end
 
 @implementation MLPlaceholderViewController
@@ -17,14 +19,54 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    NSNotificationCenter* nc = [NSNotificationCenter defaultCenter];
+    [nc addObserver:self selector:@selector(updateBackground) name:kMonalBackgroundChanged object:nil];
 }
 
 
 -(void) viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     self.splitViewController.preferredDisplayMode=UISplitViewControllerDisplayModeAllVisible;
+    
+    [self updateBackground];
 }
 
+-(void) viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
 
+-(void) updateBackground
+{
+    BOOL backgrounds = [[HelperTools defaultsDB] boolForKey:@"ChatBackgrounds"];
+    
+    if(backgrounds)
+    {
+        self.backgroundImageView.hidden = NO;
+        NSString* imageName = [[HelperTools defaultsDB] objectForKey:@"BackgroundImage"];
+        if(imageName)
+        {
+            if([imageName isEqualToString:@"CUSTOM"])
+            {
+                self.backgroundImageView.image = [[MLImageManager sharedInstance] getBackground];
+            }
+            else
+            {
+                self.backgroundImageView.image = [UIImage imageNamed:imageName];
+            }
+        }
+        else
+        {
+            self.backgroundImageView.hidden = YES;
+        }
+    }
+    else
+    {
+        self.backgroundImageView.hidden = YES;
+    }
+}
 
 @end

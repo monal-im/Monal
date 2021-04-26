08e609d05d543475b00ad16da9a675d9ea8afd17
JimTsai <poormusic2001@gmail.com>
2021-04-26 18:50:56 +0800

    add exact version info (build time / build id) to about window #618 (#619)
    * add exact version info (build time / build id) to about window #618

---
+++ Monal/Classes/MLSettingsAboutViewController.h
@@ -0,0 +1,17 @@
+//
+//  MLSettingsAboutViewController.h
+//  Monal
+//
+//  Created by jimtsai (poormusic2001@gmail.com) on 2021/4/12.
+//  Copyright © 2021 Monal.im. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MLSettingsAboutViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END

79c6ab904f43f9570c73a3b51c5148b20ec4c8a9
Thilo Molitor <thilo@eightysoft.de>
2021-05-10 17:12:12 +0200

    Allow selection of "system sound" for alerts

--- Monal/Classes/MLSoundsTableViewController.m
+++ Monal/Classes/MLSoundsTableViewController.m
@@ -13,86 +13,95 @@
 @import AVFoundation;
 
 @interface MLSoundsTableViewController ()
-@property (nonatomic, strong) NSArray *soundList;
+@property (nonatomic, strong) NSArray* soundList;
 @property (nonatomic, assign) NSUInteger selectedIndex;
-@property (nonatomic, strong) AVAudioPlayer *audioPlayer;
+@property (nonatomic, strong) AVAudioPlayer* audioPlayer;
 @end
 
 @implementation MLSoundsTableViewController
 
-
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.title=NSLocalizedString(@"Sounds",@ "");
-    
-    self.soundList = @[@"Morse",
-                       @"Xylophone",
-                       @"Bloop",
-                       @"Bing",
-                       @"Pipa",
-                       @"Water",
-                       @"Forest",
-                       @"Echo",
-                       @"Area 51",
-                       @"Wood",
-                       @"Chirp",
-                       @"Sonar",
-                       ];
-    
+    self.title = NSLocalizedString(@"Sounds", @"");
+    self.soundList = @[
+        @"System Sound",
+        @"Morse",
+        @"Xylophone",
+        @"Bloop",
+        @"Bing",
+        @"Pipa",
+        @"Water",
+        @"Forest",
+        @"Echo",
+        @"Area 51",
+        @"Wood",
+        @"Chirp",
+        @"Sonar",
+    ];
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+-(NSInteger) numberOfSectionsInTableView:(UITableView*) tableView
+{
     return 2;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+-(NSInteger) tableView:(UITableView*) tableView numberOfRowsInSection:(NSInteger) section
+{
     if(section==0)
         return 1;
     else
         return self.soundList.count;
 }
 
--(NSString *) tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+-(NSString*) tableView:(UITableView*) tableView titleForHeaderInSection:(NSInteger) section
 {
-    if(section==1){
-        return NSLocalizedString(@"Select sounds that are played with new message notifications. Default is Xylophone.",@ "");
-    } else return nil;
+    if(section == 1)
+        return NSLocalizedString(@"Select sounds that are played with new message notifications. Default is Xylophone.", @"");
+    return nil;
 }
 
--(NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section
+-(NSString*) tableView:(UITableView*) tableView titleForFooterInSection:(NSInteger) section
 {
-    if(section==1){
-        return NSLocalizedString(@"Sounds courtesy Emrah",@ "") ;
-    } else return nil;
+    if(section==1)
+        return NSLocalizedString(@"Sounds courtesy Emrah", @"");
+    return nil;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+-(UITableViewCell*) tableView:(UITableView*) tableView cellForRowAtIndexPath:(NSIndexPath*) indexPath
+{
     if(indexPath.section == 0)
     {
-        MLSettingCell* cell=[[MLSettingCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"AccountCell"];
-        cell.parent= self;
-        cell.switchEnabled=YES;
-        cell.defaultKey=@"Sound";
-        cell.textLabel.text=NSLocalizedString(@"Play Sounds",@ "");
+        MLSettingCell* cell = [[MLSettingCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"AccountCell"];
+        cell.parent = self;
+        cell.switchEnabled = YES;
+        cell.defaultKey = @"Sound";
+        cell.textLabel.text = NSLocalizedString(@"Play Sounds", @"");
         return cell;
     }
     else
     {
-        UITableViewCell* cell=[tableView dequeueReusableCellWithIdentifier:@"soundCell"];
-        cell.textLabel.text= self.soundList[indexPath.row];
-            NSString *filename =[NSString stringWithFormat:@"alert%ld", (long)indexPath.row+1];
-        if([filename isEqualToString:[[HelperTools defaultsDB] objectForKey:@"AlertSoundFile"]]) {
-            cell.accessoryType=UITableViewCellAccessoryCheckmark;
-            self.selectedIndex= indexPath.row;
-        } else  {
-            cell.accessoryType=UITableViewCellAccessoryNone;
+        UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"soundCell"];
+        cell.textLabel.text = self.soundList[indexPath.row];
+        NSString* filename = [NSString stringWithFormat:@"alert%ld", (long)indexPath.row];
+        if(
+            (indexPath.row == 0 && [[HelperTools defaultsDB] objectForKey:@"AlertSoundFile"] == nil) ||
+            [filename isEqualToString:[[HelperTools defaultsDB] objectForKey:@"AlertSoundFile"]]
+        )
+        {
+            cell.accessoryType = UITableViewCellAccessoryCheckmark;
+            self.selectedIndex = indexPath.row;
+        }
+        else
+        {
+            cell.accessoryType = UITableViewCellAccessoryNone;
         }
         return cell;
     }
@@ -101,23 +110,30 @@
 
 -(void) playSound:(NSInteger ) index
 {
-    NSString *filename =[NSString stringWithFormat:@"alert%ld", (long)index+1];
-    NSURL *url = [[NSBundle mainBundle] URLForResource:filename withExtension:@"aif" subdirectory:@"AlertSounds"];
-    self.audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL: url error:NULL];
+    NSString* filename = [NSString stringWithFormat:@"alert%ld", (long)index];
+    NSURL* url = [[NSBundle mainBundle] URLForResource:filename withExtension:@"aif" subdirectory:@"AlertSounds"];
+    self.audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:NULL];
     [self.audioPlayer play];
 }
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [tableView deselectRowAtIndexPath:indexPath animated:NO];
-    if(indexPath.section==0) return;
+    if(indexPath.section==0)
+        return;
     
-    [self playSound:indexPath.row];
-    NSString *filename =[NSString stringWithFormat:@"alert%ld", (long)indexPath.row+1];
-    [[HelperTools defaultsDB] setObject:filename forKey:@"AlertSoundFile"];
-    NSIndexPath *old = [NSIndexPath indexPathForRow:self.selectedIndex inSection:1];
-    self.selectedIndex= indexPath.row;
-    NSArray *rows =@[old,indexPath];
+    NSString* filename = nil;
+    if(indexPath.row > 0)
+    {
+        [self playSound:indexPath.row];
+        filename = [NSString stringWithFormat:@"alert%ld", (long)indexPath.row];
+        [[HelperTools defaultsDB] setObject:filename forKey:@"AlertSoundFile"];
+    }
+    else
+        [[HelperTools defaultsDB] removeObjectForKey:@"AlertSoundFile"];
+    NSIndexPath* old = [NSIndexPath indexPathForRow:self.selectedIndex inSection:1];
+    self.selectedIndex = indexPath.row;
+    NSArray* rows = @[old, indexPath];
     [tableView reloadRowsAtIndexPaths:rows withRowAnimation:UITableViewRowAnimationNone];
 }
 

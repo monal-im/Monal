ead178b30e1ffa42bef4ed1b177944491cac3e0d
Thilo Molitor <thilo@eightysoft.de>
2021-05-23 05:41:32 +0200

    Close settings on account deletion
    The old code did not leave the edit account dialogue after the account
    was deleted (you could even saveadeleted account or try to change its
    settings)

--- Monal/Classes/XMPPEdit.m
+++ Monal/Classes/XMPPEdit.m
@@ -345,7 +345,7 @@
         [[MLNotificationQueue currentQueue] postNotificationName:kMonalRefresh object:nil userInfo:nil];
 
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            [self.navigationController popViewControllerAnimated:YES];
+            [self dismissViewControllerAnimated:YES completion:nil];
         });
     }];
     [questionAlert addAction:noAction];

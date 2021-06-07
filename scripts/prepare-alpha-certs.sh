06649652dec0b442daa98ba701546b51c010f6e4
Friedrich Altheide <11352905+FriedrichAltheide@users.noreply.github.com>
2021-06-07 18:15:50 +0200

    Update prepare-alpha-certs.sh

--- scripts/prepare-alpha-certs.sh
+++ scripts/prepare-alpha-certs.sh
@@ -20,12 +20,12 @@ security unlock-keychain -p travis ios-build.keychain
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 # Add certificates to keychain and allow codesign to access them
 echo "Importing 'apple.cer' into keychain..."
-security import apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign > /dev/null
 for file in *.p12; do
 	cert="${file%%.p12}"
 	echo "Importing '$cert.p12' and '$cert.cer' into keychain..."
-	security import $cert.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign 
-	security import $cert.p12 -k ~/Library/Keychains/ios-build.keychain -P 1234 -T /usr/bin/codesign
+	security import $cert.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign  > /dev/null
+	security import $cert.p12 -k ~/Library/Keychains/ios-build.keychain -P 1234 -T /usr/bin/codesign > /dev/null
 done
 # Set Key partition list
 security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain

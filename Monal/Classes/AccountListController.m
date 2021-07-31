f995a222ac2680fbc6a4f599f9fb8f8c0f5ade0b
Thilo Molitor <thilo@eightysoft.de>
2021-07-31 19:25:27 +0200

    Show correct auto-updated account state in settings

--- Monal/Classes/AccountListController.m
+++ Monal/Classes/AccountListController.m
@@ -94,7 +94,7 @@
     if([[account objectForKey:@"enabled"] boolValue] == YES)
     {
         cell.imageView.image = [UIImage systemImageNamed:@"checkmark.circle"];
-        if([[MLXMPPManager sharedInstance] isAccountForIdConnected: [NSString stringWithFormat:@"%@", [[self.accountList objectAtIndex:accNo] objectForKey:@"account_id"]]])
+        if([[MLXMPPManager sharedInstance] isAccountForIdConnected:[NSString stringWithFormat:@"%@", [[self.accountList objectAtIndex:accNo] objectForKey:@"account_id"]]])
         {
             accessory.image = [UIImage imageNamed:@"Connected"];
             cell.accessoryView = accessory;

e43d1c9d7f845bc8e374346abb2f5123df006372
Friedrich Altheide <11352905+FriedrichAltheide@users.noreply.github.com>
2021-08-12 07:24:04 +0200

    fix bindResource migration for older clients

--- Monal/Classes/xmpp.m
+++ Monal/Classes/xmpp.m
@@ -2509,14 +2509,16 @@ NSString *const kData=@"data";
 
 -(void) bindResource:(NSString*) resource
 {
+    if(resource == nil)
+        return [self bindResource:[HelperTools encodeRandomResource]];
     //check if our resource is a modern one and change it to a modern one if not
     //this should fix rare bugs when monal was first installed a long time ago when the resource didn't yet had a random part
     NSArray* parts = [resource componentsSeparatedByString:@"."];
-    if([[HelperTools dataWithHexString:parts[1]] length] < 1)
+    if([parts count] < 2 || [[HelperTools dataWithHexString:parts[1]] length] < 1)
         return [self bindResource:[HelperTools encodeRandomResource]];
     
     _accountState = kStateBinding;
-    XMPPIQ* iqNode =[[XMPPIQ alloc] initWithType:kiqSetType];
+    XMPPIQ* iqNode = [[XMPPIQ alloc] initWithType:kiqSetType];
     [iqNode setBindWithResource:resource];
     [self sendIq:iqNode withHandler:$newHandler(MLIQProcessor, handleBind)];
 }

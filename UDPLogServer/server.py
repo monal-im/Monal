bce66fa453c10bf5efd08ad06e269f075a7b0a00
Thilo Molitor <thilo@eightysoft.de>
2021-08-15 19:35:55 +0200

    Add colored output to udp log server install deb pkg python3-termcolor
    You can use:
    pip3 install termcolor
    or:
    pip install termcolor

--- UDPLogServer/server.py
+++ UDPLogServer/server.py
@@ -7,13 +7,35 @@ import json
 import zlib
 import hashlib
 
-try:
-    from Cryptodome.Cipher import AES  # pycryptodomex
-except ImportError:
-    from Crypto.Cipher import AES  # pycryptodome
-
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
+
+# import optional/alternative modules
+try:
+    from termcolor import cprint
+except ImportError as e:
+    eprint(e)
+    def cprint(text, color=None, on_color=None, attrs=None, **kwargs):
+        print(text, **kwargs)
+try:
+    from Cryptodome.Cipher import AES  # pycryptodomex
+except ImportError as e:
+    from Crypto.Cipher import AES  # pycryptodome
+
+def flag_to_kwargs(flag):
+    kwargs = {}
+    if flag != None:
+        if flag & 1:     # error
+            kwargs = {"color": "red", "on_color": "on_grey", "attrs": ["bold"]}
+        elif flag & 2:   # warning
+            kwargs = {"color": "yellow", "on_color": "on_grey", "attrs": []}
+        elif flag & 4:   # info
+            kwargs = {"color": "grey", "attrs": ["bold", "dark"]}
+        elif flag & 8:   # debug
+            kwargs = {"color": "grey", "attrs": ["bold"]}
+        elif flag & 16:  # verbose
+            kwargs = {"color": "white", "attrs": []}
+    return kwargs
 
 def decrypt(ciphertext, key):
     iv = ciphertext[:12]
@@ -41,6 +63,7 @@ parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, 
 parser.add_argument("-k", "--key", type=str, required=True, metavar='KEY', help="AES-Key to use for decription of incoming data")
 parser.add_argument("-l", "--listen", type=str, metavar='HOSTNAME', help="Local hostname or IP to listen on (Default: :: e.g. any)", default="::")
 parser.add_argument("-p", "--port", type=int, metavar='PORT', help="Port to listen on (Default: 5555)", default=5555)
+parser.add_argument("-f", "--file", type=str, required=False, metavar='FILE', help="Filename to write the log to (in addition to stdout)")
 args = parser.parse_args()
 
 # "derive" 256 bit key
@@ -51,6 +74,19 @@ key = m.digest()
 # create listening udp socket and process all incoming packets
 sock = socket.socket(socket.AF_INET6 if ipaddress.ip_address(args.listen).version==6 else socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((args.listen, args.port))
+last_counter = None
+
+logfd = None
+if args.file:
+    eprint("Opening logfile '%s' for writing..." % args.file)
+    logfd = open(args.file, "w")
+
+#for level in (1, 2, 4, 8, 16):
+    #kwargs = flag_to_kwargs(level)
+    #logline = ("Test flag %d" % level).rstrip()
+    #cprint(logline, flush=True, **kwargs)
+#cprint("Test counter jump", color="yellow", on_color="on_grey", attrs=["bold"]);
+
 while True:
     # receive raw udp packet
     payload, client_address = sock.recvfrom(65536)
@@ -68,5 +104,17 @@ while True:
     # decode raw json encoded data
     decoded = json.loads(str(payload, "UTF-8"))
     
+    # check if _counter jumped over some lines
+    if last_counter != None and decoded["_counter"] != last_counter + 1:
+        logline = "counter jumped from %d to %d leaving out %d lines" % (last_counter, decoded["_counter"], decoded["_counter"] - last_counter)
+        print(logline, file=logfd)
+        cprint(logline, color="white", on_color="on_grey", attrs=["dark", "bold"]);
+    
+    # deduce log color from loglevel
+    kwargs = flag_to_kwargs(decoded["flag"] if "flag" in decoded else None)
+    
     # print original formatted log message
-    print(decoded["formattedMessage"], end="", flush=True)
+    logline = ("%s" % str(decoded["formattedMessage"])).rstrip()
+    print(logline, file=logfd)
+    cprint(logline, flush=True, **kwargs)
+    last_counter = decoded["_counter"]

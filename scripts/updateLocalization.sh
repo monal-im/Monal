ac772744808db0dd6f701ffb8a763fa38ef3a8b2
Your Name <you@example.com>
2021-10-25 04:26:52 +0200

    Updated translations

--- scripts/updateLocalization.sh
+++ scripts/updateLocalization.sh
@@ -3,20 +3,29 @@
 cd "$(dirname "$0")"
 cd ../Monal
 
+for folder in "localization/external" "shareSheet-iOS/localization/external"; do
+    git fetch origin
+    git reset --hard origin/main
+done
+
 # Run bartycrouch
 # https://github.com/Flinesoft/BartyCrouch#exclude-specific-views--nslocalizedstrings-from-localization
 if which bartycrouch > /dev/null; then
-       bartycrouch update -x
-       bartycrouch lint -x
+    bartycrouch update -x
+    bartycrouch lint -x
 else
     echo "warning: BartyCrouch not installed, download it from https://github.com/Flinesoft/BartyCrouch"
 fi
 
 for folder in "localization/external" "shareSheet-iOS/localization/external"; do
-       for file in $folder/*.lproj/*.strings; do
-               # Remove empty lines
-               sed -i '' '/^$/d' $file
-               # Remove default comments that are not supported by weblate
-               sed -i '' '/^\/\* No comment provided by engineer\. \*\/$/d' $file
-       done
+    for file in $folder/*.lproj/*.strings; do
+            # Remove empty lines
+            sed -i '' '/^$/d' $file
+            # Remove default comments that are not supported by weblate
+            sed -i '' '/^\/\* No comment provided by engineer\. \*\/$/d' $file
+    done
+    git add -u
+    git status
+    git commit -m "Updated translations"
+    git push
 done

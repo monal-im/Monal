737ba435336a70e6adf4295bb020bfd861a9afa4
Thilo Molitor <thilo@eightysoft.de>
2021-11-29 08:14:41 +0100

    Make SwiftUI reactive to KVO compliant objc classes

--- Monal/Classes/ContactDetailsInterface.swift
+++ Monal/Classes/ContactDetailsInterface.swift
@@ -9,6 +9,8 @@
 import Foundation
 import SwiftUI
 import monalxmpp
+import Combine
+import CocoaLumberjack
 
 class SheetDismisserProtocol: ObservableObject {
     weak var host: UIHostingController<AnyView>? = nil
@@ -17,12 +19,64 @@ class SheetDismisserProtocol: ObservableObject {
     }
 }
 
+class KVOObserver: NSObject {
+    var obj: NSObject
+    var keyPath: String
+    var objectWillChange: ()->Void
+    
+    init(obj:NSObject, keyPath:String, objectWillChange: @escaping ()->Void) {
+        self.obj = obj
+        self.keyPath = keyPath
+        self.objectWillChange = objectWillChange
+        super.init()
+        self.obj.addObserver(self, forKeyPath: keyPath, options: [], context: nil)
+    }
+    
+    deinit {
+        self.obj.removeObserver(self, forKeyPath:self.keyPath)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
+        self.objectWillChange()
+    }
+}
+
+@dynamicMemberLookup
+class ObservableKVOWrapper<ObjType:NSObject>: ObservableObject {
+    public var obj: ObjType
+    private var observedMembers: NSMutableSet = NSMutableSet()
+    private var observers: [KVOObserver] = Array()
+    
+    init(_ obj: ObjType) {
+        self.obj = obj
+    }
+    
+    subscript<T>(dynamicMember member: String) -> T {
+        if(!self.observedMembers.contains(member)) {
+            DDLogDebug("Adding observer for member \(member)")
+            self.observers.append(KVOObserver(obj:self.obj, keyPath:member, objectWillChange: {
+                DDLogDebug("Observer said \(member) has changed")
+                DispatchQueue.main.async {
+                    DDLogDebug("Calling self.objectWillChange.send()...")
+                    self.objectWillChange.send()
+                }
+            }))
+            self.observedMembers.add(member)
+        }
+        DDLogDebug("Returning value for member \(member)")
+        return self.obj.value(forKey:member) as! T
+    }
+}
+
 @objc
 class ContactDetailsInterface: NSObject {
-    @objc func makeContactDetails(_ contact: MLContact) -> UIViewController {
-        let details = ContactDetails(contact:contact)
-        let host = UIHostingController(rootView: AnyView(details))
+    @objc
+    func makeContactDetails(_ contact: MLContact) -> UIViewController {
+        let delegate = SheetDismisserProtocol()
+        let details = ContactDetails(delegate:delegate, contact:ObservableKVOWrapper<MLContact>(contact))
+        //let details = ContactDetails(delegate:delegate, contact:contact)
+        let host = UIHostingController(rootView:AnyView(details))
         details.delegate.host = host
-        return host;
+        return host
     }
 }

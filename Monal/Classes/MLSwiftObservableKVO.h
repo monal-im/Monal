463b0d2a6a50706705038bd929f844c503617188
Thilo Molitor <thilo@eightysoft.de>
2021-11-29 07:32:58 +0100

    Make DDLogXXX() available in swift

---
+++ Monal/Classes/MLSwiftObservableKVO.h
@@ -0,0 +1,13 @@
+//
+//  MLSwiftObservableKVO.h
+//  monalxmpp
+//
+//  Created by ich on 29.11.21.
+//  Copyright © 2021 Monal.im. All rights reserved.
+//
+
+#ifndef MLSwiftObservableKVO_h
+#define MLSwiftObservableKVO_h
+
+
+#endif /* MLSwiftObservableKVO_h */
---
+++ Monal/Classes/MLSwiftObservableKVO.m
@@ -0,0 +1,9 @@
+//
+//  MLSwiftObservableKVO.m
+//  monalxmpp
+//
+//  Created by ich on 29.11.21.
+//  Copyright © 2021 Monal.im. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>

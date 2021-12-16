monal-im/Monal

commit 85a0ff69fa25fe6bc77b383950610812a48b17d7

Allow generics in MLHandler definitions

Monal/Classes/MLConstants.h
@@ -47,8 +47,15 @@ typedef enum NotificationPrivacySettingOption {
 //some useful macros
 #define weakify(var) __weak __typeof__(var) AHKWeak_##var = var
 #define strongify(var) _Pragma("clang diagnostic push") _Pragma("clang diagnostic ignored \"-Wshadow\"") __strong __typeof__(var) var = AHKWeak_##var; _Pragma("clang diagnostic pop")
-#define nilWrapper(var) (var ? var : [NSNull null])
+#define nilWrapper(var) (var == nil ? var : [NSNull null])
 #define nilExtractor(var) (var == [NSNull null] ? nil : var)
+
+//see https://stackoverflow.com/a/62984543/3528174
+#define STRIP_PARENTHESES(X) __ESC(__ISH X)
+#define __ISH(...) __ISH __VA_ARGS__
+#define __ESC(...) __ESC_(__VA_ARGS__)
+#define __ESC_(...) __VAN ## __VA_ARGS__
+#define __VAN__ISH
 
 #if defined(IS_ALPHA) || defined(DEBUG)
     #define unreachable() { \

Monal/Classes/MLHandler.h
@@ -105,7 +105,7 @@ $invalidate(h, $BOOL(done, YES))
 #define $HANDLER(name, ...)                                               metamacro_if_eq(0, metamacro_argcount(__VA_ARGS__))( @#name : nilWrapper(name) )( _packID(name, __VA_ARGS__) )
 
 //declare handler, the order of provided arguments does not matter because we use named arguments
-#define $$class_handler(name, ...)                                              +(void) MLHandler_##name##_withArguments:(NSDictionary*) _callerArgs andBoundArguments:(NSDictionary*) _boundArgs { metamacro_if_eq(0, metamacro_argcount(__VA_ARGS__))( )( metamacro_foreach(_expand_import, ;, __VA_ARGS__) );
+#define $$class_handler(name, ...)                                        +(void) MLHandler_##name##_withArguments:(NSDictionary*) _callerArgs andBoundArguments:(NSDictionary*) _boundArgs { metamacro_if_eq(0, metamacro_argcount(__VA_ARGS__))( )( metamacro_foreach(_expand_import, ;, __VA_ARGS__) );
 #define $$instance_handler(name, instance, ...)                           +(void) MLHandler_##name##_withArguments:(NSDictionary*) _callerArgs andBoundArguments:(NSDictionary*) _boundArgs { metamacro_if_eq(0, metamacro_argcount(__VA_ARGS__))( )( metamacro_foreach(_expand_import, ;, __VA_ARGS__) ); [instance MLInstanceHandler_##name##_withArguments:_callerArgs andBoundArguments:_boundArgs]; } -(void) MLInstanceHandler_##name##_withArguments:(NSDictionary*) _callerArgs andBoundArguments:(NSDictionary*) _boundArgs { metamacro_if_eq(0, metamacro_argcount(__VA_ARGS__))( )( metamacro_foreach(_expand_import, ;, __VA_ARGS__) );
 #define $_ID(type, var)                                                   type var __unused = _callerArgs[@#var] ? _callerArgs[@#var] : _boundArgs[@#var]
 #define $_BOOL(var)                                                       BOOL var __unused = _callerArgs[@#var] ? [_callerArgs[@#var] boolValue] : [_boundArgs[@#var] boolValue]
@@ -120,7 +120,8 @@ $invalidate(h, $BOOL(done, YES))
 #define $invalidate(handler, ...)                                         [handler invalidateWithArguments:@{ __VA_ARGS__ }]
 
 //internal stuff
-#define _expand_import(num, param)                                        param
+#define _expand_import(num, param)                                        _expand_import_inner(param)
+#define _expand_import_inner(X)                                           STRIP_PARENTHESES(X)
 #define _packID(name, value, ...)                                         @#name : nilWrapper(value)
 #define _packBOOL(name, value, ...)                                       @#name : [NSNumber numberWithBool: value ]
 #define _packINT(name, value, ...)                                        @#name : [NSNumber numberWithInt: value ]

Monal/Classes/MLPubSub.m
@@ -325,7 +325,7 @@ static NSDictionary* _defaultOptions;
     ] andData:nil]];
     //only add publish-options if present
     if([configOptions count] > 0)
-        [[query findFirst:@"{http://jabber.org/protocol/pubsub}pubsub"] addChild:[[MLXMLNode alloc] initWithElement:@"publish-options" withAttributes:@{} andChildren:@[
+        [(MLXMLNode*)[query findFirst:@"{http://jabber.org/protocol/pubsub}pubsub"] addChild:[[MLXMLNode alloc] initWithElement:@"publish-options" withAttributes:@{} andChildren:@[
             [[XMPPDataForm alloc] initWithType:@"submit" formType:@"http://jabber.org/protocol/pubsub#publish-options" andDictionary:configOptions]
         ] andData:nil]];
     [_account sendIq:query withHandler:$newHandler(self, handlePublishResult,

Monal/Classes/MLPubSubProcessor.m
@@ -31,7 +31,7 @@
 
 @implementation MLPubSubProcessor
 
-$$class_handler(avatarHandler, $_ID(xmpp*, account), $_ID(NSString*, jid), $_ID(NSString*, type), $_ID(NSDictionary*, data))
+$$class_handler(avatarHandler, $_ID(xmpp*, account), $_ID(NSString*, jid), $_ID(NSString*, type), $_ID((NSDictionary<NSString*, MLXMLNode*>*), data))
     DDLogDebug(@"Got new avatar metadata from '%@'", jid);
     if([type isEqualToString:@"publish"])
     {
@@ -77,7 +77,7 @@ $$class_handler(avatarHandler, $_ID(xmpp*, account), $_ID(NSString*, jid), $_ID(
     }
 $$
 
-$$class_handler(handleAvatarFetchResult, $_ID(xmpp*, account), $_BOOL(success), $_ID(NSString*, jid), $_ID(XMPPIQ*, errorIq), $_ID(XMPPIQ*, errorReason), $_ID(NSDictionary*, data))
+$$class_handler(handleAvatarFetchResult, $_ID(xmpp*, account), $_BOOL(success), $_ID(NSString*, jid), $_ID(XMPPIQ*, errorIq), $_ID(XMPPIQ*, errorReason), $_ID((NSDictionary<NSString*, MLXMLNode*>*), data))
     //ignore errors here (e.g. simply don't update the avatar image)
     //(this should never happen if other clients and servers behave properly)
     if(!success)
@@ -99,7 +99,7 @@ $$class_handler(handleAvatarFetchResult, $_ID(xmpp*, account), $_BOOL(success), 
     }
 $$
 
-$$class_handler(rosterNameHandler, $_ID(xmpp*, account), $_ID(NSString*, jid), $_ID(NSString*, type), $_ID(NSDictionary*, data))
+$$class_handler(rosterNameHandler, $_ID(xmpp*, account), $_ID(NSString*, jid), $_ID(NSString*, type), $_ID((NSDictionary<NSString*, MLXMLNode*>*), data))
     //new/updated nickname
     if([type isEqualToString:@"publish"])
     {
@@ -156,7 +156,7 @@ $$class_handler(rosterNameHandler, $_ID(xmpp*, account), $_ID(NSString*, jid), $
     }
 $$
 
-$$class_handler(bookmarksHandler, $_ID(xmpp*, account), $_ID(NSString*, jid), $_ID(NSString*, type), $_ID(NSDictionary*, data))
+$$class_handler(bookmarksHandler, $_ID(xmpp*, account), $_ID(NSString*, jid), $_ID(NSString*, type), $_ID((NSDictionary<NSString*, MLXMLNode*>*), data))
     if(![jid isEqualToString:account.connectionProperties.identity.jid])
     {
         DDLogWarn(@"Ignoring bookmarks update not coming from our own jid");
@@ -256,7 +256,7 @@ $$class_handler(bookmarksHandler, $_ID(xmpp*, account), $_ID(NSString*, jid), $_
     }
 $$
 
-$$class_handler(handleBookarksFetchResult, $_ID(xmpp*, account), $_BOOL(success), $_ID(XMPPIQ*, errorIq), $_ID(NSString*, errorReason), $_ID(NSDictionary*, data))
+$$class_handler(handleBookarksFetchResult, $_ID(xmpp*, account), $_BOOL(success), $_ID(XMPPIQ*, errorIq), $_ID(NSString*, errorReason), $_ID((NSDictionary<NSString*, MLXMLNode*>*), data))
     if(!success)
     {
         //item-not-found means: no bookmarks in storage --> use an empty data dict

commit e5861ebb859fd32a9d75872b7554f1603f621547

Fix crash introduced in commit 85a0ff69fa25fe6bc77b383950610812a48b17d7

Monal/Classes/DataLayer.m
@@ -253,16 +253,17 @@ static NSDateFormatter* dbFormatter;
         NSString* query = @"UPDATE account SET server=?, other_port=?, username=?, resource=?, domain=?, enabled=?, directTLS=?, rosterName=?, statusMessage=? WHERE account_id=?;";
         NSString* server = (NSString*)[dictionary objectForKey:kServer];
         NSString* port = (NSString*)[dictionary objectForKey:kPort];
-        NSArray* params = @[server == nil ? @"" : server,
-                        port == nil ? @"5222" : port,
-                        ((NSString*)[dictionary objectForKey:kUsername]),
-                        ((NSString*)[dictionary objectForKey:kResource]),
-                        ((NSString*)[dictionary objectForKey:kDomain]),
-                        [dictionary objectForKey:kEnabled],
-                        [dictionary objectForKey:kDirectTLS],
-                        [dictionary objectForKey:kRosterName] ? ((NSString*)[dictionary objectForKey:kRosterName]) : @"",
-                        [dictionary objectForKey:@"statusMessage"] ? ((NSString*)[dictionary objectForKey:@"statusMessage"]) : @"",
-                        [dictionary objectForKey:kAccountID]
+        NSArray* params = @[
+            server == nil ? @"" : server,
+            port == nil ? @"5222" : port,
+            ((NSString*)[dictionary objectForKey:kUsername]),
+            ((NSString*)[dictionary objectForKey:kResource]),
+            ((NSString*)[dictionary objectForKey:kDomain]),
+            [dictionary objectForKey:kEnabled],
+            [dictionary objectForKey:kDirectTLS],
+            [dictionary objectForKey:kRosterName] ? ((NSString*)[dictionary objectForKey:kRosterName]) : @"",
+            [dictionary objectForKey:@"statusMessage"] ? ((NSString*)[dictionary objectForKey:@"statusMessage"]) : @"",
+            [dictionary objectForKey:kAccountID]
         ];
         BOOL result = [self.db executeNonQuery:query andArguments:params];
 //         // insert self chat for omemo foreign key

Monal/Classes/MLConstants.h
@@ -47,8 +47,8 @@ typedef enum NotificationPrivacySettingOption {
 //some useful macros
 #define weakify(var) __weak __typeof__(var) AHKWeak_##var = var
 #define strongify(var) _Pragma("clang diagnostic push") _Pragma("clang diagnostic ignored \"-Wshadow\"") __strong __typeof__(var) var = AHKWeak_##var; _Pragma("clang diagnostic pop")
-#define nilWrapper(var) (var == nil ? var : [NSNull null])
-#define nilExtractor(var) (var == [NSNull null] ? nil : var)
+#define nilWrapper(var)   (var == nil           ? [NSNull null] : var)
+#define nilExtractor(var) (var == [NSNull null] ? nil           : var)
 
 //see https://stackoverflow.com/a/62984543/3528174
 #define STRIP_PARENTHESES(X) __ESC(__ISH X)

Monal/Classes/MLHandler.m
@@ -92,7 +92,8 @@
         //default arguments of the caller
         [inv setArgument:(void* _Nonnull)&args atIndex:2];
         //bound arguments of the handler
-        NSDictionary* boundArgs = _internalData[@"boundArguments"];
+        //make sure we use a copy because we don't want to leak changes of our bound arguments dict into already running invocations
+        NSDictionary* boundArgs = [_internalData[@"boundArguments"] copy];
         [inv setArgument:(void* _Nonnull)&boundArgs atIndex:3];
         //now call it
         [inv invoke];

Monal/Classes/MLIQProcessor.m
@@ -224,9 +224,9 @@ $$class_handler(handleBind, $_ID(xmpp*, account), $_ID(XMPPIQ*, iqNode))
         return;
     }
     
-    DDLogInfo(@"Now bound to full jid: %@", [iqNode findFirst:@"{urn:ietf:params:xml:ns:xmpp-bind}bind/jid#"]);
+    DDLogInfo(@"Now bound to fullJid: %@", [iqNode findFirst:@"{urn:ietf:params:xml:ns:xmpp-bind}bind/jid#"]);
     [account.connectionProperties.identity bindJid:[iqNode findFirst:@"{urn:ietf:params:xml:ns:xmpp-bind}bind/jid#"]];
-    DDLogDebug(@"jid=%@, resource=%@, fullJid=%@", account.connectionProperties.identity.jid, account.connectionProperties.identity.resource, account.connectionProperties.identity.fullJid);
+    DDLogDebug(@"bareJid=%@, resource=%@, fullJid=%@", account.connectionProperties.identity.jid, account.connectionProperties.identity.resource, account.connectionProperties.identity.fullJid);
     
     //update resource in db (could be changed by server)
     NSMutableDictionary* accountDict = [[NSMutableDictionary alloc] initWithDictionary:[[DataLayer sharedInstance] detailsForAccount:account.accountNo]];
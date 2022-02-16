f82d67439c9f5140e46ae6a9e8b9c111b95490ef
Friedrich Altheide <11352905+FriedrichAltheide@users.noreply.github.com>
2022-02-16 09:24:10 +0100

    beta.build: appstore + export macOS version

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+function buildAndExportMacOS {
+    local APP_DEVELOPER_NAME="$1"
+    local EXPORT_OPTIONS_CATALYST="$2"
+
+    echo ""
+    echo "***************************"
+    echo "*     Archiving macOS     *"
+    echo "***************************"
+    xcrun xcodebuild -workspace "Monal.xcworkspace" -scheme "Monal" -sdk macosx -configuration $BUILD_TYPE -destination 'generic/platform=macOS,variant=Mac Catalyst,name=Any Mac' -archivePath "build/macos_$APP_NAME.xcarchive" archive CODE_SIGN_IDENTITY="$APP_DEVELOPER_NAME" CODE_SIGN_STYLE="Manual" GCC_PREPROCESSOR_DEFINITIONS="$GCC_PREPROCESSOR_DEFINITIONS" BUILD_LIBRARIES_FOR_DISTRIBUTION=YES SUPPORTS_MACCATALYST=YES
+
+    xcodebuild -exportArchive -archivePath "build/macos_$APP_NAME.xcarchive" -exportPath "build/app" -exportOptionsPlist "$EXPORT_OPTIONS_CATALYST" CODE_SIGN_STYLE="Manual"
+    echo "build dir:"
+    ls -l "build"
+}
+
 # Abort on Error
 set -e
 
@@ -30,23 +45,25 @@ echo "***************************************"
 pod install --repo-update
 
 echo ""
-echo "***************************"
-echo "*     Archiving macOS     *"
-echo "***************************"
-xcrun xcodebuild -workspace "Monal.xcworkspace" -scheme "Monal" -sdk macosx -configuration $BUILD_TYPE -destination 'generic/platform=macOS,variant=Mac Catalyst,name=Any Mac' -archivePath "build/macos_$APP_NAME.xcarchive" archive CODE_SIGN_IDENTITY="$APP_DEVELOPER_NAME" CODE_SIGN_STYLE="Manual" GCC_PREPROCESSOR_DEFINITIONS="$GCC_PREPROCESSOR_DEFINITIONS" BUILD_LIBRARIES_FOR_DISTRIBUTION=YES SUPPORTS_MACCATALYST=YES
-
-echo ""
 echo "****************************"
 echo "*     Exporting macOS      *"
 echo "****************************"
 # see: https://gist.github.com/cocoaNib/502900f24846eb17bb29
 # and: https://forums.developer.apple.com/thread/100065
 # and: for developer-id distribution (distribution *outside* of appstore) an developer-id certificate must be used for building
-xcodebuild -exportArchive -archivePath "build/macos_$APP_NAME.xcarchive" -exportPath "build/app" -exportOptionsPlist $EXPORT_OPTIONS_CATALYST CODE_SIGN_STYLE="Manual"
-echo "build dir:"
-ls -l "build"
+if [ ! -z ${EXPORT_OPTIONS_CATALYST_APPSTORE} ]; then
+    echo "***************************************"
+    echo "*    Exporting AppStore macOS         *"
+    echo "***************************************"
+    buildAndExportMacOS "$APP_DEVELOPER_NAME_APPSTORE" "$EXPORT_OPTIONS_CATALYST_APPSTORE"
+fi
 
-if [ -z ${SKIP_CATALYST_ZIP_CREATION+x} ]; then
+if [ ! -z ${EXPORT_OPTIONS_CATALYST_APP_EXPORT} ]; then
+    echo "***********************************"
+    echo "*    Exporting app macOS          *"
+    echo "***********************************"
+    buildAndExportMacOS "$APP_DEVELOPER_NAME_APP_EXPORT" "$EXPORT_OPTIONS_CATALYST_APP_EXPORT"
+
     echo ""
     echo "**************************"
     echo "*     Packing macOS zip  *"

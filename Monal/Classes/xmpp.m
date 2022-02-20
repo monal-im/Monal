fb48b5e942a8581640858459f6de93239026b691
Friedrich Altheide <11352905+FriedrichAltheide@users.noreply.github.com>
2022-02-20 10:25:56 +0100

    Revert "Fix parse queue freezing"
    This reverts commit 33b22d19d63a115f39559fe163268f8a87c3e94f.

--- Monal/Classes/xmpp.m
+++ Monal/Classes/xmpp.m
@@ -640,23 +640,21 @@ NSString* const kStanza = @"stanza";
 
 -(void) freezeParseQueue
 {
-    _parseQueue.suspended = YES;
     //this has to be synchronous because we want to be sure no further stanzas are leaking from the parse queue
     //into the receive queue once we leave this method
-    //NOTE: make sure we do a real async dispatch not using the shortcut in dispatchOnReceiveQueue: because that could cause races
-    [_receiveQueue addOperations:@[[NSBlockOperation blockOperationWithBlock:^{
+    _parseQueue.suspended = YES;
+    [self dispatchOnReceiveQueue: ^{
         DDLogWarn(@"Parse queue is freezed now!");
-    }]] waitUntilFinished:YES];
+    }];
 }
 
 -(void) unfreezeParseQueue
 {
     //this has to be synchronous because we want to be sure the parse queue is operating again once we leave this method
-    //NOTE: make sure we do a real async dispatch not using the shortcut in dispatchOnReceiveQueue: because that could cause races
-    [_receiveQueue addOperations:@[[NSBlockOperation blockOperationWithBlock:^{
-        self->_parseQueue.suspended = NO;
+    [self dispatchOnReceiveQueue: ^{
+        _parseQueue.suspended = NO;
         DDLogWarn(@"Parse queue is UNfreezed now!");
-    }]] waitUntilFinished:YES];
+    }];
 }
 
 -(void) unfreezed

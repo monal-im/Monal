32aa46c76844e7daa7351ebf374c9f0ad57097b7
Thilo Molitor <thilo@eightysoft.de>
2022-05-04 19:26:30 +0200

    Use new LoginView written in SwiftUI

--- Monal/Classes/ActiveChatsViewController.m
+++ Monal/Classes/ActiveChatsViewController.m
@@ -20,6 +20,7 @@
 #import "MLXEPSlashMeHandler.h"
 #import "MLNotificationQueue.h"
 #import "MLSettingsAboutViewController.h"
+#import <Monal-Swift.h>
 
 @import QuartzCore.CATransaction;
 
@@ -365,7 +366,8 @@ static NSMutableSet* _smacksWarningDisplayed;
 {
     // display quick start if the user never seen it or if there are 0 enabled accounts
     if(![[HelperTools defaultsDB] boolForKey:@"HasSeenLogin"] || [[DataLayer sharedInstance] enabledAccountCnts].intValue == 0) {
-        [self performSegueWithIdentifier:@"showLogin" sender:self];
+        UIViewController* loginViewController = [[SwiftuiInterface new] makeViewWithName:@"WelcomeLogIn"];
+        [self presentViewController:loginViewController animated:YES completion:^{}];
         return;
     }
     if(![[HelperTools defaultsDB] boolForKey:@"HasSeenPrivacySettings"]) {

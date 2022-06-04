78435249f8087346618aa5f82d6a2f4ad5075ce4
Thilo Molitor <thilo@eightysoft.de>
2022-06-04 04:12:26 +0200

    Fix nilWrapper macro compiler issues

--- Monal/Classes/MLConstants.h
+++ Monal/Classes/MLConstants.h
@@ -51,7 +51,7 @@ typedef enum NotificationPrivacySettingOption {
 //some useful macros
 #define weakify(var)                        __weak __typeof__(var) AHKWeak_##var = var
 #define strongify(var)                      _Pragma("clang diagnostic push") _Pragma("clang diagnostic ignored \"-Wshadow\"") __strong __typeof__(var) var = AHKWeak_##var; _Pragma("clang diagnostic pop")
-#define nilWrapper(var)                     (var == nil           ? [NSNull null] : var)
+#define nilWrapper(var)                     (var == nil           ? (id)[NSNull null] : (id)var)
 #define nilExtractor(var)                   (var == [NSNull null] ? nil           : var)
 #define nilDefault(var, def)                (var == nil ? def : var)
 #define emptyDefault(var, eq, def)          (var == nil || [var isEqual:eq] ? def : var)

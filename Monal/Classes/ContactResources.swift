289c641254e6699d8941918a3046046d13bb43e0
Thilo Molitor <thilo@eightysoft.de>
2022-07-24 04:35:21 +0200

    Fix software version info ui update

--- Monal/Classes/ContactResources.swift
+++ Monal/Classes/ContactResources.swift
@@ -21,23 +21,19 @@ func resourceRowElement(_ k: String, _ v: String, space: CGFloat = 5) -> some Vi
 
 struct ContactResources: View {
     @StateObject var contact: ObservableKVOWrapper<MLContact>
-    @State var contactVersionInfos: OrderedDictionary<String, MLContactSoftwareVersionInfo>
+    @State var contactVersionInfos: [String:ObservableKVOWrapper<MLContactSoftwareVersionInfo>]
 
-    init(contact: ObservableKVOWrapper<MLContact>, previewMock: OrderedDictionary<String, MLContactSoftwareVersionInfo>? = nil) {
+    init(contact: ObservableKVOWrapper<MLContact>, previewMock: [String:ObservableKVOWrapper<MLContactSoftwareVersionInfo>]? = nil) {
         _contact = StateObject(wrappedValue: contact)
         
         if previewMock != nil {
             self.contactVersionInfos = previewMock!
         } else {
-            var tmpInfos = OrderedDictionary<String, MLContactSoftwareVersionInfo>()
+            var tmpInfos:[String:ObservableKVOWrapper<MLContactSoftwareVersionInfo>] = [:]
             for ressourceName in DataLayer.sharedInstance().resources(for: contact.obj) {
-                // query software version from contact
-                MLXMPPManager.sharedInstance()
-                    .getEntitySoftWareVersion(for: contact.obj, andResource: ressourceName)
-
                 // load already known software version info from database
                 if let softwareInfo = DataLayer.sharedInstance().getSoftwareVersionInfo(forContact: contact.obj.contactJid, resource: ressourceName, andAccount: contact.obj.accountId) {
-                    tmpInfos[ressourceName] = softwareInfo
+                    tmpInfos[ressourceName] = ObservableKVOWrapper<MLContactSoftwareVersionInfo>(softwareInfo)
                 }
             }
             self.contactVersionInfos = tmpInfos
@@ -46,23 +42,37 @@ struct ContactResources: View {
 
     var body: some View {
         List {
-            ForEach(Array(self.contactVersionInfos.keys), id: \.self) { resourceKey in
-                if let versionInfo = self.contactVersionInfos[resourceKey] {
+            ForEach(self.contactVersionInfos.sorted(by:{ $0.0 < $1.0 }), id: \.key) { key, value in
+                if let versionInfo = value {
                     Section {
-                        resourceRowElement("Resource:", versionInfo.resource)
-                        resourceRowElement("Client Name:", versionInfo.appName)
-                        resourceRowElement("Client Version:", versionInfo.appVersion)
-                        resourceRowElement("OS:", versionInfo.platformOs)
+                        resourceRowElement("Resource:", versionInfo.resource ?? "")
+                        resourceRowElement("Client Name:", versionInfo.appName ?? "")
+                        resourceRowElement("Client Version:", versionInfo.appVersion ?? "")
+                        resourceRowElement("OS:", versionInfo.platformOs ?? "")
                     }
                 }
             }
-            .onReceive(NotificationCenter.default.publisher(for: NSNotification.Name("kMonalXmppUserSoftWareVersionRefresh")).receive(on: RunLoop.main)) { notification in
-                DDLogVerbose("Got software version info...")
-                if let obj = notification.object as? xmpp, let softwareInfo = notification.userInfo?["versionInfo"] as? MLContactSoftwareVersionInfo {
-                    if softwareInfo.fromJid == contact.obj.contactJid && obj.accountNo == contact.obj.accountId {
-                        DDLogVerbose("Successfully matched software version info update to current contact")
-                        self.contactVersionInfos[softwareInfo.resource] = softwareInfo
+        }
+        .onReceive(NotificationCenter.default.publisher(for: NSNotification.Name("kMonalXmppUserSoftWareVersionRefresh")).receive(on: RunLoop.main)) { notification in
+            if let xmppAccount = notification.object as? xmpp, let softwareInfo = notification.userInfo?["versionInfo"] as? MLContactSoftwareVersionInfo {
+                DDLogVerbose("Got software version info from account \(xmppAccount)...")
+                if softwareInfo.fromJid == contact.obj.contactJid && xmppAccount.accountNo == contact.obj.accountId {
+                    DispatchQueue.main.async {
+                        DDLogVerbose("Successfully matched software version info update to current contact: \(contact.obj)")
+                        self.contactVersionInfos[softwareInfo.resource] = ObservableKVOWrapper<MLContactSoftwareVersionInfo>(softwareInfo)
                     }
+                }
+            }
+        }
+        .onAppear {
+            DDLogVerbose("View will appear...")
+            let newTimeout = DispatchTime.now() + 1.0;
+            DispatchQueue.main.asyncAfter(deadline: newTimeout) {
+                DDLogVerbose("Refreshing software version info...")
+                for ressourceName in DataLayer.sharedInstance().resources(for: contact.obj) {
+                    // query software version from contact
+                    MLXMPPManager.sharedInstance()
+                        .getEntitySoftWareVersion(for: contact.obj, andResource: ressourceName)
                 }
             }
         }
@@ -70,11 +80,11 @@ struct ContactResources: View {
     }
 }
 
-func previewMock() -> OrderedDictionary<String, MLContactSoftwareVersionInfo> {
-    var previewMock: OrderedDictionary<String, MLContactSoftwareVersionInfo> = OrderedDictionary()
-    previewMock["m1"] = MLContactSoftwareVersionInfo.init(jid: "test1@monal.im", andRessource: "m1", andAppName: "Monal", andAppVersion: "1.1.1", andPlatformOS: "ios")
-    previewMock["m2"] = MLContactSoftwareVersionInfo.init(jid: "test1@monal.im", andRessource: "m2", andAppName: "Monal", andAppVersion: "1.1.2", andPlatformOS: "macOS")
-    previewMock["m3"] = MLContactSoftwareVersionInfo.init(jid: "test1@monal.im", andRessource: "m3", andAppName: "Monal", andAppVersion: "1.1.2", andPlatformOS: "macOS")
+func previewMock() -> [String:ObservableKVOWrapper<MLContactSoftwareVersionInfo>] {
+    var previewMock:[String:ObservableKVOWrapper<MLContactSoftwareVersionInfo>] = [:]
+    previewMock["m1"] = ObservableKVOWrapper<MLContactSoftwareVersionInfo>(MLContactSoftwareVersionInfo.init(jid: "test1@monal.im", andRessource: "m1", andAppName: "Monal", andAppVersion: "1.1.1", andPlatformOS: "ios"))
+    previewMock["m2"] = ObservableKVOWrapper<MLContactSoftwareVersionInfo>(MLContactSoftwareVersionInfo.init(jid: "test1@monal.im", andRessource: "m2", andAppName: "Monal", andAppVersion: "1.1.2", andPlatformOS: "macOS"))
+    previewMock["m3"] = ObservableKVOWrapper<MLContactSoftwareVersionInfo>(MLContactSoftwareVersionInfo.init(jid: "test1@monal.im", andRessource: "m3", andAppName: "Monal", andAppVersion: "1.1.2", andPlatformOS: "macOS"))
     return previewMock
 }
 

faddf79590410337c902b6000fe2f0b04adc1204
Thilo Molitor <thilo@eightysoft.de>
2022-07-26 19:27:03 +0200

    next part

--- Monal/Classes/MLVoIPProcessor.m
+++ Monal/Classes/MLVoIPProcessor.m
@@ -301,7 +301,13 @@ static NSMutableDictionary* _pendingCalls;
         NSUUID* uuid = [self getUUIDForCallID:callID onAccount:account];
         if(uuid == nil)
         {
-            DDLogWarn(@"Ignoring incoming SDP for unknown call: %@", callID);
+            //not found --> ignore incoming sdp and respond with error
+            DDLogWarn(@"Could not find pending call for remote sdp offer, responding with error...");
+            XMPPIQ* errorIq = [[XMPPIQ alloc] initAsErrorTo:iqNode];
+            [errorIq addChildNode:[[MLXMLNode alloc] initWithElement:@"error" withAttributes:@{@"type": @"wait"} andChildren:@[
+                [[MLXMLNode alloc] initWithElement:@"unexpected-request" andNamespace:@"urn:ietf:params:xml:ns:xmpp-stanzas"],
+            ] andData:nil]];
+            [account send:errorIq];
             return;
         }
         DDLogInfo(@"%@: Got remote SDP for call %@: %@", account, callID, resultSDP);
@@ -334,14 +340,6 @@ static NSMutableDictionary* _pendingCalls;
             }
         }];
     }
-    
-    //not found --> ignore incoming sdp and respond with error
-    DDLogWarn(@"Could not find pending call for remote sdp offer, responding with error...");
-    XMPPIQ* errorIq = [[XMPPIQ alloc] initAsErrorTo:iqNode];
-    [errorIq addChildNode:[[MLXMLNode alloc] initWithElement:@"error" withAttributes:@{@"type": @"wait"} andChildren:@[
-        [[MLXMLNode alloc] initWithElement:@"unexpected-request" andNamespace:@"urn:ietf:params:xml:ns:xmpp-stanzas"],
-    ] andData:nil]];
-    [account send:errorIq];
 }
 
 -(void) connectIncomingVoIPCall:(NSUUID*) uuid

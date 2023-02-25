f1e6d57446a0c6d3c7068dc3713d6eaa166550d6
Thilo Molitor <thilo@monal-im.org>
2023-02-25 21:48:08 +0100

    Fix sync error UX improvement

--- Monal/NotificationService/NotificationService.m
+++ Monal/NotificationService/NotificationService.m
@@ -246,10 +246,6 @@
                 DDLogDebug(@"Main app not in foreground anymore, handling first push now");
         }
         
-        //this will delay the delivery of such notifications until 60 seconds after our last sync attempt failed
-        //rather than being delivered 60 seconds after our first sync attempt failed
-        [HelperTools removePendingSyncErrorNotifications];
-        
         DDLogDebug(@"locking process and connecting accounts");
         [DDLog flushLog];
         [MLProcessLock lock];
@@ -264,6 +260,10 @@
         
         //now connect all enabled accounts
         [[MLXMPPManager sharedInstance] connectIfNecessary];
+        
+        //this will delay the delivery of such notifications until 60 seconds after our last sync attempt failed
+        //rather than being delivered 60 seconds after our first sync attempt failed
+        [HelperTools removePendingSyncErrorNotifications];
     }
 }
 

d1a8cf2016712b34acc7de2e5d791ff44ac97d1b
Thilo Molitor <thilo@monal-im.org>
2024-06-25 02:30:49 +0200

    Add logging to foregrounding/backgrounding in MLXMPPManager

---
+++ Monal/Classes/MLDelayableTimer.h
@@ -0,0 +1,13 @@
+//
+//  MLDelayableTimer.h
+//  monalxmpp
+//
+//  Created by admin on 24.06.24.
+//  Copyright © 2024 monal-im.org. All rights reserved.
+//
+
+#ifndef MLDelayableTimer_h
+#define MLDelayableTimer_h
+
+
+#endif /* MLDelayableTimer_h */
---
+++ Monal/Classes/MLDelayableTimer.m
@@ -0,0 +1,9 @@
+//
+//  MLDelayableTimer.m
+//  monalxmpp
+//
+//  Created by admin on 24.06.24.
+//  Copyright © 2024 monal-im.org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>

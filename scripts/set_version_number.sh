d0c68f926d696518b0803384217e8bf2482e165f
Thilo Molitor <thilo@monal-im.org>
2024-07-22 15:07:30 +0200

    Improve version update script

--- scripts/set_version_number.sh
+++ scripts/set_version_number.sh
@@ -2,16 +2,19 @@
 
 # Abort on Error
 set -e
+set -x
 
 cd Monal
 
 echo ""
-echo "*******************************************"
-echo "*     Setting buildNumber to $buildNumber *"
-echo "*******************************************"
+echo "***************************************************"
+echo "* Setting buildNumber to $buildNumber and version to $version *"
+echo "***************************************************"
 
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "NotificationService/Info.plist"
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "shareSheet-iOS/Info.plist"
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "$APP_NAME-Info.plist"
 
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $version" "NotificationService/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $version" "shareSheet-iOS/Info.plist"
 /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $version" "$APP_NAME-Info.plist"

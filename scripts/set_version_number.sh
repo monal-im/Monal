7d661c6450a871dbba97cb72771b0218a267fb7b
Thilo Molitor <thilo@monal-im.org>
2024-07-22 15:38:41 +0200

    Fix typo in version update script

--- scripts/set_version_number.sh
+++ scripts/set_version_number.sh
@@ -2,19 +2,20 @@
 
 # Abort on Error
 set -e
-set -x
 
 cd Monal
 
 echo ""
 echo "***************************************************"
-echo "* Setting buildNumber to $buildNumber and version to $version *"
+echo "* Setting buildNumber to $buildNumber and version to $buildVersion *"
 echo "***************************************************"
+
+set -x
 
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "NotificationService/Info.plist"
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "shareSheet-iOS/Info.plist"
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "$APP_NAME-Info.plist"
 
-/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $version" "NotificationService/Info.plist"
-/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $version" "shareSheet-iOS/Info.plist"
-/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $version" "$APP_NAME-Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $buildVersion" "NotificationService/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $buildVersion" "shareSheet-iOS/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $buildVersion" "$APP_NAME-Info.plist"

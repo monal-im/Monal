31a835200facb71df7b6db4c0e0482d111760896
Thilo Molitor <thilo@monal-im.org>
2024-08-07 18:47:28 +0200

    Improve github workflows and mail2webhook script

--- scripts/mail2webhook.py
+++ scripts/mail2webhook.py
@@ -12,7 +12,7 @@ def to_camel_case(text):
     s = s.split()
     if len(text) == 0:
         return text
-    return s[0] + ''.join(i.capitalize() for i in s[1:])
+    return s[0].lower() + ''.join(i.capitalize() for i in s[1:])
 
 # parse commandline
 parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, description="Simple python script to trigger a github ")
@@ -26,7 +26,7 @@ args = parser.parse_args()
 parser = email.parser.BytesParser()
 message = parser.parse(sys.stdin.buffer)
 
-subject = message["subject"]
+subject = re.sub(r'\s+', ' ', message["subject"]).strip()
 date = message["date"]
 
 # python > 3.9 variant
@@ -45,16 +45,28 @@ if message.is_multipart():
 else:
     body = message.get_payload(decode=True)
 
-# transform body in an array of stripped strings
+# transform body into an array of stripped strings
 body = [s.strip() for s in str(body, 'UTF-8').split("\n")]
 
 # parse app properties
 properties = {to_camel_case(k.strip()): v.strip() for k, v in [line.split(": ", 1) for line in body if len(line.split(": ", 1)) > 1]}
 
-# sanity checks
-if "The following app has been approved for distribution:" not in body:
-    print("Wrong state mentioned in mail", file=sys.stderr)
+# sanity checks and state extraction
+match = re.match(r"^The status of your \((?P<platform>.+)\) app, (?P<app_name>.+), is now \"(?P<state>.+)\"$", subject)
+if match == None:
+    print(f"Mail subject does not contain proper state: '{subject}'", file=sys.stderr)
     sys.exit(0)
+state = {"_"+to_camel_case(k.strip()): v.strip() for k, v in match.groupdict().items()}
+state["_state"] = to_camel_case(state["_state"].strip())
+if state["_appName"] != properties["appName"]:
+    print(f"Mail subject states different app name than properties in mail body: stateAppName='{state['_appName']}', appName='{properties['appName']}'", file=sys.stderr)
+    sys.exit(0)
+
+# merge body properties and extracted state
+properties = state | {"_datetime": date} | properties
+#print(properties)
+
+# filter everything using the given commandline arguments
 for entry in args.filter:
     k, v = entry.split("=", 1)
     if k not in properties:

4d4464ead6f412b8b579d4cb20d1f8608a0df476
Thilo Molitor <thilo@monal-im.org>
2024-08-28 03:44:39 +0200

    Clean up code

--- Monal/shareSheet-iOS/MLSelectionController.h
+++ Monal/shareSheet-iOS/MLSelectionController.h
@@ -7,7 +7,6 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "MLContact.h"
 
 NS_ASSUME_NONNULL_BEGIN
 typedef void(^selectionResult)(NSDictionary *);
--- Monal/shareSheet-iOS/MLSelectionController.m
+++ Monal/shareSheet-iOS/MLSelectionController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MLSelectionController.h"
+#import "MLContact.h"
 
 @interface MLSelectionController ()
 

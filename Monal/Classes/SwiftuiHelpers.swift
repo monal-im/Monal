ce993e35341dbb9bf0d3502c11fe19a007fdf6fe
Matthew Fennell <matthew@fennell.dev>
2024-09-26 00:36:50 +0100

    Open swipe view with selected image in chat view
    For now, we only provide the currently selected image, so there is no
    change in user experience. But we will want to enable multiple images
    in a future commit.

--- Monal/Classes/SwiftuiHelpers.swift
+++ Monal/Classes/SwiftuiHelpers.swift
@@ -722,7 +722,7 @@ class SwiftuiInterface : NSObject {
         let delegate = SheetDismisserProtocol()
         let host = UIHostingController(rootView:AnyView(EmptyView()))
         delegate.host = host
-        host.rootView = AnyView(try! ImageViewer(delegate:delegate, info:info))
+        host.rootView = AnyView(MediaItemSwipeView(currentItem: info, allItems: [info]))
         return host
     }
     

c45c90c72eb67aabbc71a3bbc44f3fb461360007
Thilo Molitor <thilo@monal-im.org>
2024-10-07 08:06:05 +0200

    Improve power calculation script

--- scripts/power.py
+++ scripts/power.py
@@ -5,10 +5,8 @@ import re
 from datetime import datetime
 import argparse
 import statistics
-import logging
-
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)s [%(levelname)-7s] %(name)s {%(threadName)s} %(filename)s:%(lineno)d: %(message)s")
-logger = logging.getLogger(__name__)
+import logging, logging.config
+import sys
 
 def calculate_power_hour_ratio(start, end):
     time_format = "%d.%m.%y, %H:%M"
@@ -50,29 +48,72 @@ def calculate_power_ratios(data_points):
         end = data_points[i]
         power_hour_ratio = calculate_power_hour_ratio(start, end)
         if start['charging'] == False and end['charging'] == True:
-            if power_hour_ratio is None:
+            if power_hour_ratio is None or power_hour_ratio > 0:
                 ignored_discharging_periods += 1
+                logger.debug(f"Ignoring discharging period: {start} - {end} --> {power_hour_ratio}...")
                 continue
             discharging.append(power_hour_ratio)
         elif start['charging'] == True and end['charging'] == False:
-            if power_hour_ratio is None:
+            if power_hour_ratio is None or power_hour_ratio < 0:
                 ignored_charging_periods += 1
+                logger.debug(f"Ignoring charging period: {start} - {end}...")
+                continue
+            charging.append(power_hour_ratio)
+        elif start['charging'] == True and end['charging'] == True:
+            if power_hour_ratio is None or power_hour_ratio < 0:
+                ignored_charging_periods += 1
+                logger.debug(f"Ignoring charging period: {start} - {end} --> {power_hour_ratio}...")
                 continue
             charging.append(power_hour_ratio)
         else:
             logger.error(f"Unexpected ({'unusable' if power_hour_ratio is None else 'usable'}) datapoints: {start}, {end}")
             continue
     discharging_median = statistics.median(discharging) if len(discharging)>0 else 0
+    discharging_mean = statistics.mean(discharging) if len(discharging)>0 else 0
     charging_median = statistics.median(charging) if len(charging)>0 else 0
+    charging_mean = statistics.mean(charging) if len(charging)>0 else 0
     logger.info(f"Power ratios calculated: {len(discharging)+ignored_discharging_periods} discharging periods ({len(discharging)} usable), {len(charging)+ignored_charging_periods} charging periods ({len(charging)} usable)...")
-    return discharging, discharging_median, charging, charging_median
+    return discharging, discharging_median, discharging_mean, charging, charging_median, charging_mean
 
 parser = argparse.ArgumentParser(description="Process a zip file of JSON files containing power, time, and charging data.")
 parser.add_argument('--file', metavar='file.zip', required=True, help="Path to the zip file")
+parser.add_argument("--log", metavar='LOGLEVEL', help="Loglevel to log", default="INFO")
 args = parser.parse_args()
 
-data_points = process_zip(args.file)
-discharging, discharging_median, charging, charging_median = calculate_power_ratios(data_points)
+logging.config.dictConfig({
+	"version": 1,
+	"disable_existing_loggers": False,
+	
+	"formatters": {
+		"simple": {
+			"format": "%(asctime)s [%(levelname)-7s] %(name)s {%(threadName)s} %(filename)s:%(lineno)d: %(message)s",
+			"color": True
+		}
+	},
+	
+	"handlers": {
+		"stderr": {
+			"class": "logging.StreamHandler",
+			"level": args.log,
+			"formatter": "simple"
+		},
+		"ignore": {
+			"class": "logging.NullHandler",
+			"level": "DEBUG"
+		}
+	},
+	
+	"loggers": {
+		"": {
+			"level": "DEBUG",
+			"handlers": ["stderr"]
+		}
+	}
+})
+logger = logging.getLogger(__name__)
 
-print(f"Discharging ratios (median: {discharging_median:.3f}):", discharging)
-print(f"Charging ratios (median: {charging_median:.3f}):", charging)
+data_points = process_zip(args.file)
+discharging, discharging_median, discharging_mean, charging, charging_median, charging_mean = calculate_power_ratios(data_points)
+
+print(f"Discharging ratios (median: {discharging_median:.3f}, mean: {discharging_mean:.3f}):", discharging)
+print(f"Charging ratios (median: {charging_median:.3f}, mean: {charging_mean:.3f}):", charging)

04556aac946e22b9e83f85d207b9afd3fbb09921
Thilo Molitor <thilo@monal-im.org>
2024-10-15 12:24:24 +0200

    Fix sorting of data points in power consumption analysis script

--- scripts/power.py
+++ scripts/power.py
@@ -28,14 +28,13 @@ def process_zip(zip_file_path):
                 sorted_files.append((re.search(r'^[^/]+-(\d+)\.json$', file_name).group(1), file_name))
             elif re.match(r'^[^/]+\.json$', file_name):
                 sorted_files.append(("1", file_name))
-        sorted_files.sort(key=lambda x: int(x[0]))
-        print(sorted_files)
         for _, file_name in sorted_files:
             logger.debug(f"Parsing file: {file_name}")
             with zip_file.open(file_name) as json_file:
                 data = json.load(json_file)
                 data_points.append(data)
     logger.info(f"Zip file successfully processed, {len(data_points)} data-points extracted...")
+    data_points.sort(key=lambda x: datetime.strptime(x["time"], "%d.%m.%y, %H:%M"))
     return data_points
 
 def calculate_power_ratios(data_points):
@@ -60,6 +59,12 @@ def calculate_power_ratios(data_points):
                 logger.debug(f"Ignoring charging period: {start} - {end}...")
                 continue
             charging.append(power_hour_ratio)
+        elif start['charging'] == False and end['charging'] == False:
+            if power_hour_ratio is None or power_hour_ratio > 0:
+                ignored_discharging_periods += 1
+                logger.debug(f"Ignoring discharging period: {start} - {end} --> {power_hour_ratio}...")
+                continue
+            discharging.append(power_hour_ratio)
         elif start['charging'] == True and end['charging'] == True:
             if power_hour_ratio is None or power_hour_ratio < 0:
                 ignored_charging_periods += 1
@@ -116,5 +121,5 @@ logger = logging.getLogger(__name__)
 data_points = process_zip(args.file)
 discharging, discharging_median, discharging_mean, charging, charging_median, charging_mean = calculate_power_ratios(data_points)
 
-print(f"Discharging ratios (median: {discharging_median:.3f}, mean: {discharging_mean:.3f}):", discharging)
-print(f"Charging ratios (median: {charging_median:.3f}, mean: {charging_mean:.3f}):", charging)
+print(f"{args.file}: Discharging ratios (median: {discharging_median:.3f}, mean: {discharging_mean:.3f}):", discharging)
+print(f"{args.file}: Charging ratios (median: {charging_median:.3f}, mean: {charging_mean:.3f}):", charging)

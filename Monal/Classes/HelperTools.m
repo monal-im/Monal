dfb88c318fe95f55e41244aa0eecf9fc5466cb5e
Thilo Molitor <thilo@monal-im.org>
2024-11-27 04:11:07 +0100

    Use real threads for extra runloops rather than dispatch queues

--- Monal/Classes/HelperTools.m
+++ Monal/Classes/HelperTools.m
@@ -619,13 +619,13 @@ void swizzle(Class c, SEL orig, SEL new)
         runloops = [NSMutableDictionary new];
     });
     
-    //every identifier has its own thread priority/qos class
-    __block dispatch_queue_priority_t priority;
+    //every identifier has its own thread qos class
+    __block NSQualityOfService qos;
     __block char* name;
     switch(identifier)
     {
-        case MLRunLoopIdentifierNetwork: priority = DISPATCH_QUEUE_PRIORITY_BACKGROUND; name = "im.monal.runloop.networking"; break;
-        case MLRunLoopIdentifierTimer: priority = DISPATCH_QUEUE_PRIORITY_BACKGROUND; name = "im.monal.runloop.timer"; break;
+        case MLRunLoopIdentifierNetwork: qos = NSQualityOfServiceBackground; name = "im.monal.runloop.networking"; break;
+        case MLRunLoopIdentifierTimer: qos = NSQualityOfServiceBackground; name = "im.monal.runloop.timer"; break;
         default: unreachable(@"unknown runloop identifier!");
     }
     
@@ -634,9 +634,7 @@ void swizzle(Class c, SEL orig, SEL new)
         {
             NSCondition* condition = [NSCondition new];
             [condition lock];
-            dispatch_async(dispatch_queue_create_with_target(name, DISPATCH_QUEUE_SERIAL, dispatch_get_global_queue(priority, 0)), ^{
-                //set thread name, too (not only runloop name)
-                [NSThread.currentThread setName:[NSString stringWithFormat:@"%s", name]];
+            NSThread* newRunloopThread = [[NSThread alloc] initWithBlock:^{
                 //we don't need an @synchronized block around this because the @synchronized block of the outer thread
                 //waits until we signal our condition (e.g. no other thread can race with us)
                 NSRunLoop* localLoop = runloops[@(identifier)] = [NSRunLoop currentRunLoop];
@@ -648,7 +646,13 @@ void swizzle(Class c, SEL orig, SEL new)
                     [localLoop run];
                     usleep(10000);          //sleep 10ms if we ever return from our runloop to not consume too much cpu
                 }
-            });
+            }];
+            //configure and start thread
+            [newRunloopThread setName:[NSString stringWithFormat:@"%s", name]];
+            //newRunloopThread.threadPriority = 1.0;
+            newRunloopThread.qualityOfService = qos;
+            [newRunloopThread start];
+            //wait for the new thread to create a new runloop, it will immediately spin the runloop after signalling this condition
             [condition wait];
             [condition unlock];
         }

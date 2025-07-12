c0d5814d7b2596c467e5baa478d674a9986d9e74
Thilo Molitor <thilo@monal-im.org>
2025-07-13 01:53:03 +0200

    Implement light-/dark-mode distinction for app icon/logo

--- scripts/image_resize.sh
+++ scripts/image_resize.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-if [[ "$1" == "" ]]; then
-    echo "Usage: $(basename "$0") <original_logo.png> [type, for example: 'Alpha']"
+if [[ "$1" == "" || "$2" == "" ]]; then
+    echo "Usage: $(basename "$0") <original_logo.png> <original_dark_logo.png> [type, for example: 'Alpha']"
     exit 1
 fi
 
-type="$2"
+type="$3"
     
 for d in ./Images.xcassets/${type}AppIcon.appiconset ./Images.xcassets/${type}AppLogo.imageset; do
-	for png in $d/*.png; do
-		size="$(identify -format "%wx%h" "$png")"
-		echo "$png ($size)"
-		#mv "$png" "${png%.png}.old"
-		convert "$1" -alpha off -filter triangle -resize "$size" "$png"
-	done
+    for png in $d/*.png; do
+        size="$(identify -format "%wx%h" "$png")"
+        echo "$png ($size)"
+        src="$1"
+        if [[ $png =~ ^.*_[dD][aA][rR][kK]\.[pP][nN][gG]$ ]]; then
+            src="$2"
+        fi
+        #mv "$png" "${png%.png}.old"
+        convert "$src" -alpha off -filter triangle -resize "$size" "$png"
+    done
 done;

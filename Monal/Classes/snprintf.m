0f4c75d99905d21e1d830639eb847874b3b75c26
Thilo Molitor <thilo@monal-im.org>
2025-08-02 04:47:59 +0200

    Manually import commit 359763a from weiss/c99-snprintf

--- Monal/Classes/snprintf.m
+++ Monal/Classes/snprintf.m
@@ -1504,6 +1504,9 @@ myround(LDOUBLE value)
 {
 	UINTMAX_T intpart = cast(value);
 
+	if (intpart == UINTMAX_MAX)
+		return UINTMAX_MAX;
+	
 	return ((value - intpart) < 0.5) ? intpart : intpart + 1;
 }
 

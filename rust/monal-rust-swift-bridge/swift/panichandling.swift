47759c0db7f54b847d57f39345cf3077147c84e9
Thilo Molitor <thilo@monal-im.org>
2025-08-26 04:35:58 +0200

    Don't allow installing an internal rust panichandler twice

--- rust/monal-rust-swift-bridge/swift/panichandling.swift
+++ rust/monal-rust-swift-bridge/swift/panichandling.swift
@@ -1,8 +1,12 @@
 public typealias rust_panic_handler_t = @convention(block) (String, String) -> Void;
 var panicHandler: Optional<rust_panic_handler_t> = nil;
+var panicHandlerInstalled = false;
 public func setRustPanicHandler(_ ph: @escaping rust_panic_handler_t) {
     panicHandler = ph;
-    install_panichandler();
+    if !panicHandlerInstalled {
+        install_panichandler();
+        panicHandlerInstalled = true;
+    }
 }
 public func rust_panic_handler(text: RustString, backtrace: RustString) {
     if let ph = panicHandler {

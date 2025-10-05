cb576dd35eedf02095ffb7bbb90f58dc571312b1
Thilo Molitor <thilo@monal-im.org>
2025-10-05 18:26:57 +0200

    Fix speaker handling in video calls again
    The handleAudioRouteChangeNotification: handler did create a loop by
    calling self.speaker = x
    We don't need to change to/from speaker in that handler: the change has
    already been done once that handler gets called.

--- Monal/Classes/MLCall.m
+++ Monal/Classes/MLCall.m
@@ -223,10 +223,17 @@
 -(void) setSpeaker:(BOOL) speaker
 {
     @synchronized(self) {
+        DDLogError(@"*** setSpeaker:%@ called...", bool2str(speaker));
         if(self.webRTCClient == nil || self.audioSession == nil)
+        {
+            DDLogError(@"*** setSpeaker: not ready: %@, %@", self.webRTCClient, self.audioSession);
             return;
+        }
         if(_speaker == speaker)
+        {
+            DDLogError(@"*** setSpeaker: called but identical...");
             return;
+        }
         _speaker = speaker;
         if(_speaker)
             [self.webRTCClient speakerOn];
@@ -476,7 +483,10 @@
     [[RTCAudioSession sharedInstance] setIsAudioEnabled:YES];
     [[RTCAudioSession sharedInstance] unlockForConfiguration];
     if(self.callType == MLCallTypeVideo)
+    {
+        DDLogError(@"*** Activating speaker...");
         self.speaker = YES;
+    }
 }
 
 -(void) didDeactivateAudioSession:(AVAudioSession*) audioSession
@@ -1711,15 +1721,15 @@
 {
     DDLogVerbose(@"Audio route changed: %@", notification);
     DDLogVerbose(@"Current audio route: %@", self.audioSession.currentRoute);
-    BOOL speaker = NO;
-    for(AVAudioSessionPortDescription* port in self.audioSession.currentRoute.outputs)
-        if(port.portType == AVAudioSessionPortBuiltInSpeaker)
-            speaker = YES;
-    
-    if(speaker)
-        self.speaker = YES;
-    else
-        self.speaker = NO;
+//     BOOL speaker = NO;
+//     for(AVAudioSessionPortDescription* port in self.audioSession.currentRoute.outputs)
+//         if(port.portType == AVAudioSessionPortBuiltInSpeaker)
+//             speaker = YES;
+//     
+//     if(speaker)
+//         self.speaker = YES;
+//     else
+//         self.speaker = NO;
 }
 
 -(MLCallEncryptionState) encryptionTypeForDeviceid:(NSNumber* _Nonnull) deviceid

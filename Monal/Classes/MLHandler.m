a40e2e5a58871be2625b19d80d2ee340b9e6da8c
Thilo Molitor <thilo@monal-im.org>
2025-10-10 21:18:59 +0200

    Allow MLHandlers to return values (then returned by $call())

--- Monal/Classes/MLHandler.m
+++ Monal/Classes/MLHandler.m
@@ -82,7 +82,7 @@ NSString* type_to_classname(NSString* type)
     _internalData[@"boundArguments"] = [self sanitizeArguments:args];
 }
 
--(void) callWithArguments:(NSDictionary* _Nullable) args
+-(id) callWithArguments:(NSDictionary* _Nullable) args
 {
     MLAssert(_internalData[@"delegate"] && _internalData[@"handlerName"], @"Tried to call MLHandler while delegate and/or handlerName was not set!", @{@"handler": _internalData});
     [self checkInvalidation];
@@ -94,8 +94,9 @@ NSString* type_to_classname(NSString* type)
             @"delegate": _internalData[@"delegate"],
             @"handlerSelector": NSStringFromSelector(sel),
         }];
+    NSMethodSignature* sig = [delegate methodSignatureForSelector:sel];
     DDLogVerbose(@"Calling handler %@...", self);
-    NSInvocation* inv = [NSInvocation invocationWithMethodSignature:[delegate methodSignatureForSelector:sel]];
+    NSInvocation* inv = [NSInvocation invocationWithMethodSignature:sig];
     [inv setTarget:delegate];
     [inv setSelector:sel];
     //arguments 0 and 1 are self and _cmd respectively, automatically set by NSInvocation
@@ -107,12 +108,18 @@ NSString* type_to_classname(NSString* type)
     [inv setArgument:(void* _Nonnull)&boundArgs atIndex:3];
     //now call it
     [inv invoke];
+    //and extract the return value
+    __unsafe_unretained id tempObject = nil;
+    if(sig.methodReturnLength)
+        [inv getReturnValue:&tempObject];
+    id retval = tempObject;
+    return retval;
 }
 
--(void) invalidateWithArguments:(NSDictionary* _Nullable) args
+-(id) invalidateWithArguments:(NSDictionary* _Nullable) args
 {
     if(!(_internalData[@"delegate"] && _internalData[@"invalidationName"]))
-        return;
+        return nil;
     [self checkInvalidation];
     args = [self sanitizeArguments:args];
     id delegate = NSClassFromString(_internalData[@"delegate"]);
@@ -124,7 +131,8 @@ NSString* type_to_classname(NSString* type)
             @"invalidationSelector": NSStringFromSelector(sel),
         }];
     DDLogVerbose(@"Calling invalidation %@...", self);
-    NSInvocation* inv = [NSInvocation invocationWithMethodSignature:[delegate methodSignatureForSelector:sel]];
+    NSMethodSignature* sig = [delegate methodSignatureForSelector:sel];
+    NSInvocation* inv = [NSInvocation invocationWithMethodSignature:sig];
     [inv setTarget:delegate];
     [inv setSelector:sel];
     //arguments 0 and 1 are self and _cmd respectively, automatically set by NSInvocation
@@ -136,6 +144,12 @@ NSString* type_to_classname(NSString* type)
     //now call it
     [inv invoke];
     _invalidated = YES;
+    //and extract the return value
+    __unsafe_unretained id tempObject = nil;
+    if(sig.methodReturnLength)
+        [inv getReturnValue:&tempObject];
+    id retval = tempObject;
+    return retval;
 }
 
 -(NSString*) id

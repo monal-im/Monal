d1ba33a286ea7a70ea6c33bf72cb9c1c6a04ef57
Thilo Molitor <thilo@monal-im.org>
2025-12-10 11:39:14 +0100

    Improve sync error notification text

--- Monal/Classes/HelperTools.m
+++ Monal/Classes/HelperTools.m
@@ -1739,7 +1739,7 @@ void swizzle(Class c, SEL orig, SEL new)
                     }
                     DDLogWarn(@"Posting syncError notification for %@...", account.connectionProperties.identity.jid);
                     UNMutableNotificationContent* content = [UNMutableNotificationContent new];
-                    content.title = NSLocalizedString(@"Could not synchronize", @"");
+                    content.title = NSLocalizedString(@"Connectivity issues", @"");
                     content.subtitle = account.connectionProperties.identity.jid;
                     content.body = NSLocalizedString(@"Some messages might wait to be retrieved or sent. Please open the app to retry.", @"");
                     content.sound = [UNNotificationSound defaultSound];

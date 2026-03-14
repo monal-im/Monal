b89de9a53e05c54e2bbdbc663bb686ad9f5d733d
Thilo Molitor <thilo@monal-im.org>
2026-03-14 21:49:28 +0100

    Force SASL2+SCRAM for yax.im (already forced for conversations.im)

--- Monal/Classes/XMPPEdit.m
+++ Monal/Classes/XMPPEdit.m
@@ -339,8 +339,8 @@ enum DummySettingsRows {
     if(self.statusMessage)
         [dic setObject:[self.statusMessage stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] forKey:@"statusMessage"];
     
-    //conversations.im already supports sasl2 and scram ## TODO: use SCRAM preload list
-    [dic setObject:([domain.lowercaseString isEqualToString:@"conversations.im"] ? @NO : @(self.plainActivated)) forKey:kPlainActivated];
+    //conversations.im and yax,im already support sasl2 and scram ## TODO: use SCRAM preload list
+    [dic setObject:([domain.lowercaseString isEqualToString:@"yax.im"] || [domain.lowercaseString isEqualToString:@"conversations.im"] ? @NO : @(self.plainActivated)) forKey:kPlainActivated];
     
     if(!self.editMode)
     {

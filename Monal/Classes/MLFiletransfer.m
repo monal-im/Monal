98f721c7b279781a388d97a3dede7f4f52a82084
lissine <lissine@ellini.org>
2026-04-12 18:02:39 +0100

    Set the url fragment to nil instead of an empty string before downloading / checking
    When it's set to an empty string, the url has a trailing # character

--- Monal/Classes/MLFiletransfer.m
+++ Monal/Classes/MLFiletransfer.m
@@ -703,7 +703,7 @@ $$
         DDLogWarn(@"Failed to get url components, returning empty url!");
         return @"";
     }
-    urlComponents.fragment = @"";       //make sure we don't leak urlfragments to upload server
+    urlComponents.fragment = nil;       //make sure we don't leak urlfragments to upload server
     return urlComponents.string;
 }
 

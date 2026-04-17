0cac5d97f5a2035d759f5c374c822f35ea5e5981
Thilo Molitor <thilo@monal-im.org>
2026-04-17 08:53:24 +0200

    Only show contacts also listed in contact list in share sheet
    This also orders listed contacts by pinned state for easier access.

--- Monal/shareSheet-iOS/ShareViewController.m
+++ Monal/shareSheet-iOS/ShareViewController.m
@@ -76,7 +76,22 @@
     // list all contacts, not only active chats
     // that will clutter the list of selectable contacts, but you can always use sirikit interactions
     // to get the recently used contacts listed
-    self.recipients = [[DataLayer sharedInstance] contactList];
+    NSArray<MLContact*>* allContacts = [[DataLayer sharedInstance] contactList];
+#ifdef IS_QUICKSY
+    self.recipients = allContacts;
+#else
+    NSMutableArray<MLContact*>* contactsToDisplay = [NSMutableArray new];
+    //ignore all contacts not at least in any roster state: e.g. subscribedTo or asking state
+    //OR is subscribedFrom (e.g. we approved them already, but they don't approve us)
+    //order pinned before unpinned ones
+    for(MLContact* contact in allContacts)
+        if(((contact.isSubscribedTo || contact.hasOutgoingContactRequest) || contact.isSubscribedFrom) && contact.isPinned)
+            [contactsToDisplay addObject:contact];
+    for(MLContact* contact in allContacts)
+        if(((contact.isSubscribedTo || contact.hasOutgoingContactRequest) || contact.isSubscribedFrom) && !contact.isPinned)
+            [contactsToDisplay addObject:contact];
+    self.recipients = [contactsToDisplay copy];
+#endif
     self.accounts = [[DataLayer sharedInstance] enabledAccountList];
 
     if(self.intentContact != nil)

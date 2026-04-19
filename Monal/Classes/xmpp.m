e47153e3d7bb69b2e4f9684847521cef516b0803
Thilo Molitor <thilo@monal-im.org>
2026-04-19 08:23:04 +0200

    Fix static ping delay timer being global to all xmpp instances

--- Monal/Classes/xmpp.m
+++ Monal/Classes/xmpp.m
@@ -97,6 +97,7 @@ NSString* const kStanza = @"stanza";
     //internal handlers and flags
     MLDelayableTimer* _loginTimer;
     MLDelayableTimer* _pingTimer;
+    monal_void_block_t _pingDelayTimer;
     MLDelayableTimer* _reconnectTimer;
     NSMutableArray* _timersToCancelOnDisconnect;
     NSMutableArray* _smacksAckHandler;
@@ -1545,8 +1546,6 @@ NSString* const kStanza = @"stanza";
 
 -(void) sendPing:(double) timeout
 {
-    static monal_void_block_t delayTimer = nil;        //it doesn't matter if this has a race condition
-    
     DDLogVerbose(@"sendPing called");
     [self dispatchAsyncOnReceiveQueue: ^{
         DDLogVerbose(@"sendPing called - now inside receiveQueue");
@@ -1580,18 +1579,18 @@ NSString* const kStanza = @"stanza";
         }
         else if([self->_parseQueue operationCount] > 4)
         {
-            if(delayTimer != nil)
+            if(self->_pingDelayTimer != nil)
                 DDLogWarn(@"Ping already delayed, ignoring additional ping...");
             else
             {
                 DDLogWarn(@"parseQueue overflow, delaying ping by 4 seconds.");
-                delayTimer = createTimer(4.0, (^{
-                    [self removeTimerToCancelOnDisconnect:delayTimer];
-                    delayTimer = nil;
+                self->_pingDelayTimer = createTimer(4.0, (^{
+                    [self removeTimerToCancelOnDisconnect:self->_pingDelayTimer];
+                    self->_pingDelayTimer = nil;
                     DDLogDebug(@"ping delay expired, retrying ping.");
                     [self sendPing:timeout];
                 }));
-                [self addTimerToCancelOnDisconnect:delayTimer];
+                [self addTimerToCancelOnDisconnect:self->_pingDelayTimer];
             }
         }
         else

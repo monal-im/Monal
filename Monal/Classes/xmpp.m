1f505a6e2a6fac9a5753cfdc90c1ebf0a5a429e4
Thilo Molitor <thilo@monal-im.org>
2026-04-24 00:24:51 +0200

    Fix handling of smacks counting errors

--- Monal/Classes/xmpp.m
+++ Monal/Classes/xmpp.m
@@ -1920,10 +1920,13 @@ static NSRegularExpression* fastTokenRemovalRegex;
             
             @synchronized(_stateLockObject) {
                 //remove acked messages
-                [self removeAckedStanzasFromQueue:h];
-
-                self.smacksRequestInFlight = NO;        //ack returned
-                [self requestSMAck:NO];                 //request ack again (will only happen if queue is not empty)
+                //don't try to continue, if removeAckedStanzasFromQueue returned an error (it will trigger a reconnect in these cases)
+                BOOL error = [self removeAckedStanzasFromQueue:h];
+                if(!error)
+                {
+                    self.smacksRequestInFlight = NO;        //ack returned
+                    [self requestSMAck:NO];                 //request ack again (will only happen if queue is not empty)
+                }
             }
         }
         else if([parsedStanza check:@"/{jabber:client}presence"] && self.accountState >= kStateInitStarted)
@@ -2463,7 +2466,9 @@ static NSRegularExpression* fastTokenRemovalRegex;
             
             @synchronized(_stateLockObject) {
                 //remove already delivered stanzas and resend the (still) unacked ones
-                [self removeAckedStanzasFromQueue:h];
+                //but don't try to continue, if removeAckedStanzasFromQueue returned an error (it will trigger a reconnect in these cases)
+                if([self removeAckedStanzasFromQueue:h])
+                    return;
                 [self resendUnackedStanzas];
             }
             
